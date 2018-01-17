a04ad22ca419cf37ae93046c9b8c6f0a5689c4d3
HYu <yuhang.neu@gmail.com>
2018-01-17 12:00:34 -0500

    Update configWinVS14.bat
    Make directory install\bin before copying

--- configWinVS14.bat
+++ configWinVS14.bat
@@ -47,6 +47,10 @@ CD build\win
 cmake -G "Visual Studio 14 2015 Win64" ..\.. -DBOOST_ROOT=%BOOST_ROOT%
 CD ..\..
 
+echo # create install\bin folder
+if exist "install\bin" RMDIR /S /Q install\bin
+MKDIR install\bin
+
 echo # copy boost
 COPY %BOOST_ROOT%\lib64-msvc-14.0\boost_filesystem-vc140-mt-1_63.dll install\bin
 COPY %BOOST_ROOT%\lib64-msvc-14.0\boost_filesystem-vc140-mt-gd-1_63.dll install\bin
