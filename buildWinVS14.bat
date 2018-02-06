aa99877429358c57fcbf20a7259ddc7ce9c09992
Adrian Pop <adrian.pop@liu.se>
2018-02-06 23:04:35 +0100

    use VS140COMNTOOLS instead of hardcoded path

--- buildWinVS14.bat
+++ buildWinVS14.bat
@@ -1,7 +1,7 @@
 @echo off
 REM run this on wsl using "cmd.exe /C buildWinVS14.bat"
 
-@call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+@call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
 
 echo # build OMSimulator
 msbuild.exe "build\win\INSTALL.vcxproj" /t:Build /p:configuration=Release
