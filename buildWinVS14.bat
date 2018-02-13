230618f19b5be94a3bbb1bdcadc7ef5051bb9c57
Lennart Ochel <lennart.ochel@liu.se>
2018-02-13 10:57:31 +0100

    Unify VS bat files

--- buildWinVS14.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM run this on wsl using "cmd.exe /C buildWinVS14.bat"
-
-@call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
-
-echo # build OMSimulator
-msbuild.exe "build\win\INSTALL.vcxproj" /t:Build /p:configuration=Release
--- buildWinVS15.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM run this on wsl using "cmd.exe /C buildWinVS15.bat"
-
-SET "VSCMD_START_DIR=%CD%"
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-
-echo # build OMSimulator
-msbuild.exe "build\win\INSTALL.vcxproj" /t:Build /p:configuration=Release
