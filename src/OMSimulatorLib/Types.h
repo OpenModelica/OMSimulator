c85523d5ce55a7f6f0298f17191229a7c9665980
Lennart Ochel <lennart.ochel@liu.se>
2018-02-26 17:00:16 +0100

    Add more geometry types from the SSP draft

--- src/OMSimulatorLib/Types.h
+++ src/OMSimulatorLib/Types.h
@@ -119,7 +119,54 @@ typedef struct {
   const char* message;
 } oms_message_t;
 
-/** ssd:ElementGeometry */
+/**
+ * 5.2.1.1 ssd:ConnectorGeometry
+ *
+ * This optional XML element gives the geometry information of the connector.
+ * Note that x and y coordinates are in a special coordinate system, where 0,0
+ * is the lower-left corner of the containing model element, and 1,1 is the
+ * upper-right corner of the model element, regardless of aspect ratio.
+ *
+ * For systems the placement of connectors for the inside and outside view of
+ * the system is identical, the special coordinate system is just translated to
+ * different actual coordinate systems, namely the one determined by the
+ * ElementGeometry for the outside view, and the one determined by
+ * SystemGeometry for the inside view.
+ */
+typedef struct {
+  /*
+   * Required attribute giving the x coordinate of the connector inside the
+   * special coordinate system.
+   */
+  double x;
+  /*
+   * Required attribute giving the y coordinate of the connector inside the
+   * special coordinate system.
+   */
+  double y;
+} oms_connector_geometry_t;
+
+/**
+ * 5.2.2 ssd:ElementGeometry
+ *
+ * This optional XML element defines the geometry information of the model
+ * element, where (x1,y1) and (x2,y2) define the positions of the lower-left
+ * and upper-right corners of the model element in the coordinate system of its
+ * parent system. If x1>x2 this indicates horizontal flipping, y1>y2 indicates
+ * vertical flipping.
+ * The optional attribute rotation (in degrees) defines an additional rotation
+ * (applied after flipping), where positive numbers indicate left rotation
+ * (x->y). The coordinate system is oriented: x -> right, y -> up.
+ * The optional attribute iconSource defines an icon URI with the same
+ * semantics as for the source attribute of the Component element. If defined,
+ * this icon overrides any icon that may be defined e.g. in an .fmu file (as
+ * defined in the relevant FMI standards). The optional attribute iconRotation
+ * defines the rotation (in degrees) of the icon. The optional attribute
+ * iconFixedAspectRatio defines whether the icon shall be fit into the extent
+ * defined by (x1,y1), (x2,y2) and iconRotation with fixed aspect ratio. The
+ * optional attribute iconFlip defines whether any flipping indicated by
+ * (x1,y1), (x2,y2) shall be applied to the icon graphics, too.
+ */
 typedef struct {
   /**
    * Required attribute giving the x coordinate of the lower left corner of the
@@ -172,12 +219,74 @@ typedef struct {
   bool iconFixedAspectRatio;
 } oms_element_geometry_t;
 
-/** ssd:ConnectionGeometry */
+/**
+ * 5.3.2.1 ssd:ConnectionGeometry
+ *
+ * This optional element defines the geometry information of the connection.
+ * The start and end coordinates of the connection are derived automatically
+ * through the coordinates of the corresponding connectors. The only relevant
+ * geometry information provided by the connection geometry is a, by default
+ * empty, list of intermediate waypoint coordinates, which are to be interpreted
+ * as for the svg:polyline primitive, i.e. as waypoints for straight line
+ * segments, with the first and last points added automatically based on the
+ * translated coordinates of the start and end connectors. Note that x and y
+ * coordinates are in the coordinate system of the enclosing system.
+ */
 typedef struct {
+  /*
+   * Required attribute giving a list of x coordinates of the intermediate
+   * waypoints.
+   */
   double* pointsX;
+  /*
+   * Required attribute giving a list of y coordinates of the intermediate
+   * waypoints.
+   */
   double* pointsY;
+  /*
+   * Number of intermediate waypoints.
+   */
   unsigned int n;
 } oms_connection_geometry_t;
+
+/**
+ * 5.3.4 ssd:SystemGeometry
+ *
+ * This element defines the extent of the system canvas. (x1,y1) and (x2,y2)
+ * define the lower-left and upper-right corner, respectively. Different from
+ * ElementGeometry, where x1 > x2 and y1 > y2 indicate flipping, x1 < x2 and
+ * y1 < y2 MUST hold here.
+ * If undefined, the system canvas extent defaults to the bounding box of all
+ * ElementGeometry elements of the child elements of the system.
+ * When displaying the content of a sub-system together with the enclosing
+ * parent system, the transformation of co-coordinates inside the sub-system to
+ * co-ordinates in the parent system is defined by the transformation from
+ * SystemGeometry.{x1,y1,x2,y2} to ElementGeometry.{x1',y1',x2',y2'}, where
+ * ElementGeometry.z' is the respective coordinate of the sub-system when
+ * instantiated in the parent system after rotation.
+ */
+typedef struct {
+  /*
+   * Required attribute giving the x coordinate of the lower-left corner of the
+   * system canvas.
+   */
+  double x1;
+  /*
+   * Required attribute giving the y coordinate of the lower-left corner of the
+   * system canvas.
+   */
+  double y1;
+  /*
+   * Required attribute giving the x coordinate of the upper-right corner of the
+   * system canvas.
+   */
+  double x2;
+  /*
+   * Required attribute giving the y coordinate of the upper-right corner of the
+   * system canvas.
+   */
+  double y2;
+} oms_system_geometry_t;
 
 #ifdef __cplusplus
 }
