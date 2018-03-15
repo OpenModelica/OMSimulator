e6e5e9890b5fd73772c395505a9bc6cdc07bcc69
Lennart Ochel <lennart.ochel@liu.se>
2018-03-15 16:29:18 +0100

    Update batch files for VS

--- buildWinVS.bat
+++ buildWinVS.bat
@@ -1,26 +1,35 @@
-@echo off
+@ECHO off
 REM run this on wsl using:
 REM cmd.exe /C buildWinVS.bat VS15-Win64
 
-if ["%~1"]==["VS14-Win64"] SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
-if ["%~1"]==["VS14-Win32"] SET OMS_VS_VERSION="Visual Studio 14 2015"
-if ["%~1"]==["VS15-Win64"] SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
+IF ["%~1"]==["VS14-Win32"] SET OMS_VS_VERSION="Visual Studio 14 2015"
+IF ["%~1"]==["VS14-Win64"] SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
+IF ["%~1"]==["VS15-Win64"] SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
+
 
 IF NOT DEFINED OMS_VS_VERSION (
-	echo No argument or unsupported argument given. Supported: VS14-Win64, VS14-Win32, VS15-WIN64
-	echo Default to "Visual Studio 14 2015 Win64".
-	pause
-	SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
+  ECHO No argument or unsupported argument given. Use one of the following VS version strings:
+  ECHO   "VS14-Win32" for Visual Studio 14 2015
+  ECHO   "VS14-Win64" for Visual Studio 14 2015 Win64
+  ECHO   "VS15-Win64" for Visual Studio 15 2017 Win64
+  GOTO fail
 )
 
-echo Using %OMS_VS_VERSION%
+ECHO Using %OMS_VS_VERSION%
 
 SET "VSCMD_START_DIR=%CD%"
-if ["%~1"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
-if ["%~1"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-if ["%~1"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+IF ["%~1"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+IF ["%~1"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
+IF ["%~1"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 IF NOT DEFINED CMAKE_BUILD_TYPE SET CMAKE_BUILD_TYPE="Release"
 
-echo # build OMSimulator
+ECHO # build OMSimulator
 msbuild.exe "build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
+
+EXIT /b 0
+
+:fail
+ECHO Build failed!
+EXIT /b 1
--- configWinVS.bat
+++ configWinVS.bat
@@ -1,131 +1,132 @@
-@echo off
+@ECHO off
 REM run this on wsl using:
 REM cmd.exe /C configWinVS.bat VS15-Win64
 
-if ["%~1"]==["VS14-Win64"] SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
-if ["%~1"]==["VS14-Win32"] SET OMS_VS_VERSION="Visual Studio 14 2015"
-if ["%~1"]==["VS15-Win64"] SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
+IF ["%~1"]==["VS14-Win32"] SET OMS_VS_VERSION="Visual Studio 14 2015"
+IF ["%~1"]==["VS14-Win64"] SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
+IF ["%~1"]==["VS15-Win64"] SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
 
 
 IF NOT DEFINED OMS_VS_VERSION (
-	echo No argument or unsupported argument given. Supported: VS14-Win64, VS14-Win32, VS15-WIN64
-	echo Default to "Visual Studio 14 2015 Win64".
-	pause
-	SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
+  ECHO No argument or unsupported argument given. Use one of the following VS version strings:
+  ECHO   "VS14-Win32" for Visual Studio 14 2015
+  ECHO   "VS14-Win64" for Visual Studio 14 2015 Win64
+  ECHO   "VS15-Win64" for Visual Studio 15 2017 Win64
+  GOTO fail
 )
 
-echo Using %OMS_VS_VERSION%
+ECHO Using %OMS_VS_VERSION%
 
 REM default boost version
 IF NOT DEFINED BOOST_ROOT SET BOOST_ROOT=C:\local\boost_1_64_0
 
 IF NOT DEFINED CMAKE_BUILD_TYPE SET CMAKE_BUILD_TYPE="Release"
 
-SET "VSCMD_START_DIR=%CD%"
+SET VSCMD_START_DIR="%CD%"
 
-if ["%~1"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
-if ["%~1"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-if ["%~1"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+IF ["%~1"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+IF ["%~1"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
+IF ["%~1"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-echo # config fmil
-if exist "3rdParty\FMIL\build\win\" RMDIR /S /Q 3rdParty\FMIL\build\win
-if exist "3rdParty\FMIL\install\win\" RMDIR /S /Q 3rdParty\FMIL\install\win
+ECHO # config fmil
+IF EXIST "3rdParty\FMIL\build\win\" RMDIR /S /Q 3rdParty\FMIL\build\win
+IF EXIST "3rdParty\FMIL\install\win\" RMDIR /S /Q 3rdParty\FMIL\install\win
 MKDIR 3rdParty\FMIL\build\win
 CD 3rdParty\FMIL\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DFMILIB_INSTALL_PREFIX=..\..\install\win -DFMILIB_BUILD_TESTS:BOOL="0" -DFMILIB_GENERATE_DOXYGEN_DOC:BOOL="0" -DFMILIB_BUILD_STATIC_LIB:BOOL="1" -DFMILIB_BUILD_SHARED_LIB:Bool="0" -DBUILD_TESTING:BOOL="0" -DFMILIB_BUILD_BEFORE_TESTS:BOOL="0" ..\..
 CD ..\..\..\..
-echo # build fmil
+ECHO # build fmil
 msbuild.exe "3rdParty\FMIL\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # build Lua
+ECHO # build Lua
 CD 3rdParty\Lua
-if exist "install\win\" RMDIR /S /Q install\win
+IF EXIST "install\win\" RMDIR /S /Q install\win
 call buildWinVS.bat "%~1"
 CD ..\..
 
-echo # config cvode
-if exist "3rdParty\cvode\build\win\" RMDIR /S /Q 3rdParty\cvode\build\win
-if exist "3rdParty\cvode\install\win\" RMDIR /S /Q 3rdParty\cvode\install\win
+ECHO # config cvode
+IF EXIST "3rdParty\cvode\build\win\" RMDIR /S /Q 3rdParty\cvode\build\win
+IF EXIST "3rdParty\cvode\install\win\" RMDIR /S /Q 3rdParty\cvode\install\win
 MKDIR 3rdParty\cvode\build\win
 CD 3rdParty\cvode\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_INSTALL_PREFIX=..\..\install\win ..\.. -DEXAMPLES_ENABLE:BOOL="0" -DBUILD_SHARED_LIBS:BOOL="0"
 CD ..\..\..\..
-echo # build cvode
+ECHO # build cvode
 msbuild.exe "3rdParty\cvode\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # config kinsol
-if exist "3rdParty\kinsol\build\win\" RMDIR /S /Q 3rdParty\kinsol\build\win
-if exist "3rdParty\kinsol\install\win\" RMDIR /S /Q 3rdParty\kinsol\install\win
+ECHO # config kinsol
+IF EXIST "3rdParty\kinsol\build\win\" RMDIR /S /Q 3rdParty\kinsol\build\win
+IF EXIST "3rdParty\kinsol\install\win\" RMDIR /S /Q 3rdParty\kinsol\install\win
 MKDIR 3rdParty\kinsol\build\win
 CD 3rdParty\kinsol\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_INSTALL_PREFIX=..\..\install\win ..\.. -DEXAMPLES_ENABLE:BOOL="0" -DBUILD_SHARED_LIBS:BOOL="0"
 CD ..\..\..\..
-echo # build kinsol
+ECHO # build kinsol
 msbuild.exe "3rdParty\kinsol\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # config gflags
-if exist "3rdParty\gflags\build\win\" RMDIR /S /Q 3rdParty\gflags\build\win
-if exist "3rdParty\gflags\install\win\" RMDIR /S /Q 3rdParty\gflags\install\win
+ECHO # config gflags
+IF EXIST "3rdParty\gflags\build\win\" RMDIR /S /Q 3rdParty\gflags\build\win
+IF EXIST "3rdParty\gflags\install\win\" RMDIR /S /Q 3rdParty\gflags\install\win
 MKDIR 3rdParty\gflags\build\win
 CD 3rdParty\gflags\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_INSTALL_PREFIX=..\..\install\win ..\..\gflags -DBUILD_TESTING="OFF" -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
 CD ..\..\..\..
-echo # build gflags
+ECHO # build gflags
 msbuild.exe "3rdParty\gflags\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # config glog
-if exist "3rdParty\glog\build\win\" RMDIR /S /Q 3rdParty\glog\build\win
-if exist "3rdParty\glog\install\win\" RMDIR /S /Q 3rdParty\glog\install\win
+ECHO # config glog
+IF EXIST "3rdParty\glog\build\win\" RMDIR /S /Q 3rdParty\glog\build\win
+IF EXIST "3rdParty\glog\install\win\" RMDIR /S /Q 3rdParty\glog\install\win
 MKDIR 3rdParty\glog\build\win
 CD 3rdParty\glog\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_INSTALL_PREFIX=..\..\install\win ..\..\glog -DBUILD_TESTING="OFF" -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
 CD ..\..\..\..
-echo # build glog
+ECHO # build glog
 msbuild.exe "3rdParty\glog\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # config ceres-solver
-if exist "3rdParty\ceres-solver\build\win\" RMDIR /S /Q 3rdParty\ceres-solver\build\win
-if exist "3rdParty\ceres-solver\install\win\" RMDIR /S /Q 3rdParty\ceres-solver\install\win
+ECHO # config ceres-solver
+IF EXIST "3rdParty\ceres-solver\build\win\" RMDIR /S /Q 3rdParty\ceres-solver\build\win
+IF EXIST "3rdParty\ceres-solver\install\win\" RMDIR /S /Q 3rdParty\ceres-solver\install\win
 MKDIR 3rdParty\ceres-solver\build\win
 CD 3rdParty\ceres-solver\build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_INSTALL_PREFIX=..\..\install\win ..\..\ceres-solver -DCXX11="ON" -DEXPORT_BUILD_DIR="on" -DEIGEN_INCLUDE_DIR_HINTS="../../eigen/eigen" -DBUILD_EXAMPLES="OFF" -DBUILD_TESTING="OFF" -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
 CD ..\..\..\..
-echo # build ceres-solver
+ECHO # build ceres-solver
 msbuild.exe "3rdParty\ceres-solver\build\win\INSTALL.vcxproj" /t:Build /p:configuration=%CMAKE_BUILD_TYPE%
-IF NOT ["%ERRORLEVEL%"]==["0"] goto fail
+IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 
-echo # config OMSimulator
-if exist "build\win\" RMDIR /S /Q build\win
+ECHO # config OMSimulator
+IF EXIST "build\win\" RMDIR /S /Q build\win
 MKDIR build\win
 CD build\win
 cmake -G %OMS_VS_VERSION% -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..\.. -DOMFIT="ON" -DBOOST_ROOT=%BOOST_ROOT% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%
 CD ..\..\
 
 REM create install\win\bin folder
-IF NOT EXIST "install\win\bin" echo # create install\win\bin folder
+IF NOT EXIST "install\win\bin" ECHO # create install\win\bin folder
 IF NOT EXIST "install\win\bin" MKDIR "install\win\bin"
 
-echo # copy boost using CRAPPY xcopy
-set CRD=%CD%
-cd %BOOST_ROOT%
-for /d %%d in (lib*-msvc-*) do (
-  cd %%d
-  for /r %%e in (boost_system*,boost_filesystem*) do (
+ECHO # copy boost using CRAPPY xcopy
+SET CRD=%CD%
+CD %BOOST_ROOT%
+FOR /d %%d in (lib*-msvc-*) do (
+  CD %%d
+  FOR /r %%e in (boost_system*,boost_filesystem*) do (
      xcopy /Y /F %%e %CRD%\install\win\bin
   )
 )
-cd %CRD%
+CD %CRD%
 
-echo # copy lua
+ECHO # copy lua
 COPY 3rdParty\lua\install\win\lua.dll install\win\bin
 
-exit /b 0
+EXIT /b 0
 
 :fail
-echo "Something went wrong!"
-exit /b 1
+ECHO Configuring failed!
+EXIT /b 1
