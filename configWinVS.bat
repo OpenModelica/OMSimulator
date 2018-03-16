5f9d659c0eadf5d026f3207a1fcdf5d2588c7dea
Lennart Ochel <lennart.ochel@liu.se>
2018-03-16 14:52:13 +0100

    Fix bat files for 32bit builds

--- configWinVS.bat
+++ configWinVS.bat
@@ -2,10 +2,10 @@
 REM run this on wsl using:
 REM cmd.exe /C configWinVS.bat VS15-Win64
 
-IF ["%~1"]==["VS14-Win32"] SET OMS_VS_VERSION="Visual Studio 14 2015"
-IF ["%~1"]==["VS14-Win64"] SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
-IF ["%~1"]==["VS15-Win64"] SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
-
+SET OMS_VS_TARGET=%~1
+IF ["%OMS_VS_TARGET%"]==["VS14-Win32"] SET OMS_VS_PLATFORM=32 && SET OMS_VS_VERSION="Visual Studio 14 2015"
+IF ["%OMS_VS_TARGET%"]==["VS14-Win64"] SET OMS_VS_PLATFORM=64 && SET OMS_VS_VERSION="Visual Studio 14 2015 Win64"
+IF ["%OMS_VS_TARGET%"]==["VS15-Win64"] SET OMS_VS_PLATFORM=64 && SET OMS_VS_VERSION="Visual Studio 15 2017 Win64"
 
 IF NOT DEFINED OMS_VS_VERSION (
   ECHO No argument or unsupported argument given. Use one of the following VS version strings:
@@ -15,7 +15,10 @@ IF NOT DEFINED OMS_VS_VERSION (
   GOTO fail
 )
 
-ECHO Using %OMS_VS_VERSION%
+SET OMS_VS_TARGET
+SET OMS_VS_VERSION
+SET OMS_VS_PLATFORM
+ECHO.
 
 REM default boost version
 IF NOT DEFINED BOOST_ROOT SET BOOST_ROOT=C:\local\boost_1_64_0
@@ -24,9 +27,9 @@ IF NOT DEFINED CMAKE_BUILD_TYPE SET CMAKE_BUILD_TYPE="Release"
 
 SET VSCMD_START_DIR="%CD%"
 
-IF ["%~1"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-IF ["%~1"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
-IF ["%~1"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+IF ["%OMS_VS_TARGET%"]==["VS14-Win32"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+IF ["%OMS_VS_TARGET%"]==["VS14-Win64"] @call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64 8.1
+IF ["%OMS_VS_TARGET%"]==["VS15-Win64"] @call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 ECHO # config fmil
 IF EXIST "3rdParty\FMIL\build\win\" RMDIR /S /Q 3rdParty\FMIL\build\win
@@ -42,7 +45,7 @@ IF NOT ["%ERRORLEVEL%"]==["0"] GOTO fail
 ECHO # build Lua
 CD 3rdParty\Lua
 IF EXIST "install\win\" RMDIR /S /Q install\win
-call buildWinVS.bat "%~1"
+call buildWinVS.bat "%OMS_VS_TARGET%"
 CD ..\..
 
 ECHO # config cvode
@@ -114,7 +117,7 @@ IF NOT EXIST "install\win\bin" MKDIR "install\win\bin"
 ECHO # copy boost using CRAPPY xcopy
 SET CRD=%CD%
 CD %BOOST_ROOT%
-FOR /d %%d in (lib*-msvc-*) do (
+FOR /d %%d in (lib%OMS_VS_PLATFORM%*-msvc-*) do (
   CD %%d
   FOR /r %%e in (boost_system*,boost_filesystem*) do (
      xcopy /Y /F %%e %CRD%\install\win\bin
