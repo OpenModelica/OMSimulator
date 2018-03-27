96adec9b059ccf39564e14534ab58fd7342c0dc4
Bernhard Thiele <bernhard.thiele@liu.se>
2018-03-27 14:57:30 +0200

    Merged OMSysIdent Lua API into OMSimulator Lua API

--- src/OMSimulatorLua/OMSimulatorLua.c
+++ src/OMSimulatorLua/OMSimulatorLua.c
@@ -4,6 +4,10 @@
 
 #include <OMSimulator.h>
 
+#ifdef WITH_OMSYSIDENT
+#include <OMSysIdent.h>
+#endif /* WITH_OMSYSIDENT */
+
 #define REGISTER_LUA_CALL(name) lua_register(L, #name, OMSimulatorLua_##name)
 
 #ifdef _WIN32
@@ -809,6 +813,234 @@ static int OMSimulatorLua_oms2_setLogFile(lua_State *L)
   return 1;
 }
 
+/* **************************************************** */
+/* OMSysIdent API                                       */
+/* **************************************************** */
+
+#ifdef WITH_OMSYSIDENT
+//void* omsi_newSysIdentModel(void* model);
+static int OMSimulatorLua_omsi_newSysIdentModel(lua_State *L)
+{
+  if (lua_gettop(L) != 1)
+    return luaL_error(L, "expecting exactly 1 argument");
+  luaL_checktype(L, 1, LUA_TUSERDATA);
+
+  void *model = topointer(L, 1);
+
+  void *pModel = omsi_newSysIdentModel(model);
+  push_pointer(L, pModel);
+  return 1;
+}
+
+//void omsi_freeSysIdentModel(void* fitmodel);
+static int OMSimulatorLua_omsi_freeSysIdentModel(lua_State *L)
+{
+  if (lua_gettop(L) != 1)
+    return luaL_error(L, "expecting exactly 1 argument");
+  luaL_checktype(L, 1, LUA_TUSERDATA);
+
+  void *model = topointer(L, 1);
+  omsi_freeSysIdentModel(model);
+  return 0;
+}
+
+//void oms_setReal(void* model, const char* var, double value);
+// oms_status_enu_t omsi_initialize(void* fitmodel, size_t nSeries, const double* time, size_t nTime, char const* const* inputvars, size_t nInputvars, char const* const* measurementvars, size_t nMeasurementvars);
+static int OMSimulatorLua_omsi_initialize(lua_State *L)
+{
+  if (lua_gettop(L) != 5)
+    return luaL_error(L, "expecting exactly 5 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TNUMBER);   // nSeries
+  luaL_checktype(L, 3, LUA_TTABLE);    // time
+  luaL_checktype(L, 4, LUA_TTABLE);    // inputvars
+  luaL_checktype(L, 5, LUA_TTABLE);    // measurementvars
+  // luaL_checktype(L, 2, LUA_TSTRING);
+
+  void *model = topointer(L, 1);
+  size_t nSeries = lua_tonumber(L, 2);
+
+  // get size of tables
+  size_t nTime = luaL_len(L, 3);
+  size_t nInputvars = luaL_len(L, 4);
+  size_t nMeasurementvars = luaL_len(L, 5);
+  // create buffers
+  double* time = (double*) malloc(nTime*sizeof(double));
+  char const* * inputvars = (char const**) malloc(nInputvars*sizeof(char));
+  char const* * measurementvars = (char const**) malloc(nMeasurementvars*sizeof(char));
+
+  //printf("nTime: %d, nInputvars: %d, nMeasurementvars %d\n", (int)nTime, (int)nInputvars, (int)nMeasurementvars);
+
+  int i;
+  for (i=0; i < nTime; ++i) {
+    lua_rawgeti(L, 3, i+1); // push value on stack
+    time[i] = lua_tonumber(L, -1);
+    lua_pop(L, 1); // pop value from stack
+  }
+
+  for (i=0; i < nInputvars; ++i) {
+    lua_rawgeti(L, 4, i+1);
+    inputvars[i] = lua_tostring(L, -1);
+    lua_pop(L, 1);
+  }
+
+  for (i=0; i < nMeasurementvars; ++i) {
+    lua_rawgeti(L, 5, i+1);
+    measurementvars[i] = lua_tostring(L, -1);
+    lua_pop(L, 1);
+  }
+
+  oms_status_enu_t returnValue =
+    omsi_initialize(model, nSeries, time, nTime, inputvars, nInputvars, measurementvars, nMeasurementvars);
+  lua_pushinteger(L, returnValue);
+
+  free(time); free(inputvars); free(measurementvars);
+  return 1;
+}
+
+//oms_status_enu_t omsi_describe(void* fitmodel);
+static int OMSimulatorLua_omsi_describe(lua_State *L)
+{
+  if (lua_gettop(L) != 1)
+    return luaL_error(L, "expecting exactly 1 argument");
+  luaL_checktype(L, 1, LUA_TUSERDATA);
+
+  void *model = topointer(L, 1);
+  oms_status_enu_t returnValue = omsi_describe(model);
+  lua_pushinteger(L, returnValue);
+  return 1;
+}
+
+// oms_status_enu_t omsi_addMeasurement(void* fitmodel, size_t iSeries, const char* var, const double* values, size_t nValues);
+static int OMSimulatorLua_omsi_addMeasurement(lua_State *L)
+{
+  if (lua_gettop(L) != 4)
+    return luaL_error(L, "expecting exactly 4 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TNUMBER);   // iSeries
+  luaL_checktype(L, 3, LUA_TSTRING);   // var
+  luaL_checktype(L, 4, LUA_TTABLE);    // values
+
+  void *model = topointer(L, 1);
+  size_t iSeries = lua_tonumber(L, 2);
+  const char* var = lua_tostring(L, 3);
+  size_t nValues = luaL_len(L, 4);
+
+  double* values = (double*) malloc(nValues*sizeof(double));
+  int i;
+  for (i=0; i < nValues; ++i) {
+    lua_rawgeti(L, 4, i+1); // push value on stack
+    values[i] = lua_tonumber(L, -1);
+    lua_pop(L, 1); // pop value from stack
+  }
+
+  oms_status_enu_t returnValue =
+    omsi_addMeasurement(model, iSeries, var, values, nValues);
+  lua_pushinteger(L, returnValue);
+
+  free(values);
+  return 1;
+}
+
+// oms_status_enu_t omsi_addParameter(void* fitmodel, const char* var, double startvalue);
+static int OMSimulatorLua_omsi_addParameter(lua_State *L)
+{
+  if (lua_gettop(L) != 3)
+    return luaL_error(L, "expecting exactly 3 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TSTRING);   // var
+  luaL_checktype(L, 3, LUA_TNUMBER);   // startvalue
+
+  void *model = topointer(L, 1);
+  const char* var = lua_tostring(L, 2);
+  double startvalue = lua_tonumber(L, 3);
+
+  oms_status_enu_t returnValue =
+    omsi_addParameter(model, var, startvalue);
+  lua_pushinteger(L, returnValue);
+  return 1;
+}
+
+// oms_status_enu_t omsi_getParameter(void* fitmodel, const char* var, double* startvalue, double* estimatedvalue);
+static int OMSimulatorLua_omsi_getParameter(lua_State *L)
+{
+  if (lua_gettop(L) != 2)
+    return luaL_error(L, "expecting exactly 2 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TSTRING);   // var
+
+  void *model = topointer(L, 1);
+  const char* var = lua_tostring(L, 2);
+
+  double startvalue, estimatedvalue;
+  oms_status_enu_t returnValue =
+    omsi_getParameter(model, var, &startvalue, &estimatedvalue);
+  lua_pushinteger(L, returnValue);
+  lua_pushnumber(L, startvalue);
+  lua_pushnumber(L, estimatedvalue);
+  return 3;
+}
+
+// oms_status_enu_t omsi_solve(void* fitmodel, const char* reporttype);
+static int OMSimulatorLua_omsi_solve(lua_State *L)
+{
+  if (lua_gettop(L) != 2)
+    return luaL_error(L, "expecting exactly 2 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TSTRING);   // reporttype
+
+  void *model = topointer(L, 1);
+  const char* reporttype = lua_tostring(L, 2);
+
+  oms_status_enu_t returnValue =
+    omsi_solve(model, reporttype);
+  lua_pushinteger(L, returnValue);
+  return 1;
+}
+
+// oms_status_enu_t omsi_setOptions_max_num_iterations(void* fitmodel, size_t max_num_iterations);
+static int OMSimulatorLua_omsi_setOptions_max_num_iterations(lua_State *L)
+{
+  if (lua_gettop(L) != 2)
+    return luaL_error(L, "expecting exactly 2 arguments");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+  luaL_checktype(L, 2, LUA_TNUMBER);   // max_num_iterations
+
+  void *model = topointer(L, 1);
+  int max_num_iterations = lua_tointeger(L, 2);
+
+  oms_status_enu_t returnValue =
+    omsi_setOptions_max_num_iterations(model, max_num_iterations);
+  lua_pushinteger(L, returnValue);
+  return 1;
+}
+
+// oms_status_enu_t omsi_getState(void* fitmodel, omsi_fitmodelstate_t* state);
+static int OMSimulatorLua_omsi_getState(lua_State *L)
+{
+  if (lua_gettop(L) != 1)
+    return luaL_error(L, "expecting exactly 1 argument");
+  luaL_checktype(L, 1, LUA_TUSERDATA); // fitmodel
+
+  void *model = topointer(L, 1);
+
+  omsi_simodelstate_t state;
+  oms_status_enu_t returnValue =
+    omsi_getState(model, &state);
+  lua_pushinteger(L, returnValue);
+  switch (state) {
+    case omsi_simodelstate_constructed: lua_pushstring(L, "constructed"); break;
+    case omsi_simodelstate_initialized: lua_pushstring(L, "initialized"); break;
+    case omsi_simodelstate_convergence: lua_pushstring(L, "convergence"); break;
+    case omsi_simodelstate_no_convergence: lua_pushstring(L, "no_convergence"); break;
+    case omsi_simodelstate_failure: lua_pushstring(L, "failure"); break;
+  }
+  return 2;
+}
+
+#endif /* WITH_OMSYSIDENT */
+
+
 DLLEXPORT int luaopen_OMSimulatorLua(lua_State *L)
 {
   REGISTER_LUA_CALL(addConnection);
@@ -870,6 +1102,22 @@ DLLEXPORT int luaopen_OMSimulatorLua(lua_State *L)
   REGISTER_LUA_CALL(oms2_setTempDirectory);
   REGISTER_LUA_CALL(oms2_setWorkingDirectory);
   REGISTER_LUA_CALL(oms2_unloadModel);
+
+  /* ************************************ */
+  /* OMSysIdent API                       */
+  /* ************************************ */
+#ifdef WITH_OMSYSIDENT
+  REGISTER_LUA_CALL(omsi_newSysIdentModel);
+  REGISTER_LUA_CALL(omsi_freeSysIdentModel);
+  REGISTER_LUA_CALL(omsi_initialize);
+  REGISTER_LUA_CALL(omsi_describe);
+  REGISTER_LUA_CALL(omsi_addMeasurement);
+  REGISTER_LUA_CALL(omsi_addParameter);
+  REGISTER_LUA_CALL(omsi_getParameter);
+  REGISTER_LUA_CALL(omsi_solve);
+  REGISTER_LUA_CALL(omsi_setOptions_max_num_iterations);
+  REGISTER_LUA_CALL(omsi_getState);
+#endif /* WITH_OMSYSIDENT */
 
   return 0;
 }
