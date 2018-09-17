a5d8165d1428186cbdefaf35cf1122ded290d433
Lennart Ochel <lennart.ochel@liu.se>
2018-09-17 21:42:31 +0200

    Implement oms3_list

--- src/OMSimulatorLib/Model.h
+++ src/OMSimulatorLib/Model.h
@@ -52,6 +52,7 @@ namespace oms3
      */
     static Model* NewModel(const ComRef& cref);
     const ComRef& getName() const {return cref;}
+    System* getSystem(const ComRef& cref);
     oms_status_enu_t rename(const ComRef& cref);
     oms_status_enu_t list(const ComRef& cref, char** contents);
     oms_status_enu_t addSystem(const ComRef& cref, oms_system_enu_t type);
