f00ec21b2737214a83bfde9f0a6bba245f040cd5
Lennart Ochel <lennart.ochel@liu.se>
2018-10-12 14:34:47 +0200

    Remove oms3::operator+
      - OpenModelica/OMSimulator#403
      - OpenModelica/OMSimulator-testsuite#94

--- src/OMSimulatorLib/ComRef.h
+++ src/OMSimulatorLib/ComRef.h
@@ -65,7 +65,6 @@ namespace oms3
     char* cref = NULL;
   };
 
-  std::string operator+(const std::string& lhs, const ComRef& rhs);
   bool operator==(const ComRef& lhs, const ComRef& rhs);
   bool operator!=(const ComRef& lhs, const ComRef& rhs);
   bool operator<(const ComRef& lhs, const ComRef& rhs);
