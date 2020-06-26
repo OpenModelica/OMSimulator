85731d0c34280dbf9a4514f83b4392131f700164
arun3688 <rain100falls@gmail.com>
2020-06-26 16:54:28 +0200

    fix expected outputs

--- testsuite/api/test03.lua
+++ testsuite/api/test03.lua
@@ -78,7 +78,7 @@ oms_delete("test")
 -- 		</ssd:Annotations>
 -- 		<ssd:Connectors />
 -- 		<ssd:Elements>
--- 			<ssd:Component name="source" type="application/x-fmu-sharedlibrary" source="resources\0001_source.fmu">
+-- 			<ssd:Component name="source" type="application/x-fmu-sharedlibrary" source="resources/0001_source.fmu">
 -- 				<ssd:Connectors>
 -- 					<ssd:Connector name="y" kind="output">
 -- 						<ssc:Real />
@@ -127,7 +127,7 @@ oms_delete("test")
 -- 		</ssd:Annotations>
 -- 		<ssd:Connectors />
 -- 		<ssd:Elements>
--- 			<ssd:Component name="source" type="application/x-fmu-sharedlibrary" source="resources\0001_source.fmu">
+-- 			<ssd:Component name="source" type="application/x-fmu-sharedlibrary" source="resources/0001_source.fmu">
 -- 				<ssd:Connectors>
 -- 					<ssd:Connector name="y" kind="output">
 -- 						<ssc:Real />
