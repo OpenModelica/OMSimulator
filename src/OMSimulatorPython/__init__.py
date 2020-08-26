76d2c15bb4aab9cee5023d327ab0f59d2d6e3615
Lennart Ochel <lennart.ochel@liu.se>
2020-08-26 11:51:35 +0200

    Create OMSimulator package in install dir

---
+++ src/OMSimulatorPython/__init__.py
@@ -0,0 +1,10 @@
+'''OMSimulator
+
+OpenModelica FMI & TLM based simulator'''
+
+from OMSimulator.OMSimulator import OMSimulator
+
+__version__ = '2.0.1'
+__copyright__ = 'Copyright (c) 2018-CurrentYear, Open Source Modelica Consortium (OSMC),\
+                 c/o Linköpings universitet, Department of Computer and Information Science,\
+                 SE-58183 Linköping, Sweden.'
