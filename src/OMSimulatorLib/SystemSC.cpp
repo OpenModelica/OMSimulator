3f90a5b45ef63d71473c14deb2e001df802ae002
arun3688 <rain100falls@gmail.com>
2020-10-12 16:36:47 +0200

    fix condition statement

--- src/OMSimulatorLib/SystemSC.cpp
+++ src/OMSimulatorLib/SystemSC.cpp
@@ -693,7 +693,7 @@ oms_status_enu_t oms::SystemSC::updateInputs(DirectedGraph& graph)
         if (oms_status_ok != getReal(graph.getNodes()[output].getName(), value)) return oms_status_error;
         if (oms_status_ok != setReal(graph.getNodes()[input].getName(), value)) return oms_status_error;
       }
-      else if (graph.getNodes()[input].getType() == oms_signal_type_integer or graph.getNodes()[input].getType() == oms_signal_type_enum)
+      else if (graph.getNodes()[input].getType() == oms_signal_type_integer || graph.getNodes()[input].getType() == oms_signal_type_enum)
       {
         int value = 0.0;
         if (oms_status_ok != getInteger(graph.getNodes()[output].getName(), value)) return oms_status_error;
