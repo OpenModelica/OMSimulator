2029068af3d1c467b88642d0ec6639cae9618c26
arun3688 <rain100falls@gmail.com>
2020-10-14 19:33:33 +0200

    update test

--- testsuite/OMSimulator/exportSSVTemplate.lua
+++ testsuite/OMSimulator/exportSSVTemplate.lua
@@ -14,72 +14,37 @@ oms_addSystem("test.Root", oms_system_wc)
 oms_addSubModel("test.Root.Gain", "../resources/Modelica.Blocks.Math.Gain.fmu")
 oms_addSubModel("test.Root.add", "../resources/Modelica.Blocks.Math.Add.fmu")
 
-src = oms_list("test")
-print(src)
-
 oms_exportSSVTemplate("test", "template.ssv")
+
+local f = assert(io.open("template.ssv", "r"))
+local content = f:read("*all")
+print(content)
+f:close()
 
 
 -- Result:
--- <?xml version="1.0"?>
--- <ssd:SystemStructureDescription xmlns:ssc="http://ssp-standard.org/SSP1/SystemStructureCommon" xmlns:ssd="http://ssp-standard.org/SSP1/SystemStructureDescription" xmlns:ssv="http://ssp-standard.org/SSP1/SystemStructureParameterValues" xmlns:ssm="http://ssp-standard.org/SSP1/SystemStructureParameterMapping" xmlns:ssb="http://ssp-standard.org/SSP1/SystemStructureSignalDictionary" xmlns:oms="https://raw.githubusercontent.com/OpenModelica/OMSimulator/master/schema/oms.xsd" name="test" version="1.0">
--- 	<ssd:System name="Root">
--- 		<ssd:Annotations>
--- 			<ssc:Annotation type="org.openmodelica">
--- 				<oms:SimulationInformation>
--- 					<oms:FixedStepMaster description="oms-ma" stepSize="0.100000" absoluteTolerance="0.000100" relativeTolerance="0.000100" />
--- 				</oms:SimulationInformation>
--- 			</ssc:Annotation>
--- 		</ssd:Annotations>
--- 		<ssd:Connectors />
--- 		<ssd:Elements>
--- 			<ssd:Component name="add" type="application/x-fmu-sharedlibrary" source="resources/0002_add.fmu">
--- 				<ssd:Connectors>
--- 					<ssd:Connector name="u1" kind="input">
--- 						<ssc:Real />
--- 						<ssd:ConnectorGeometry x="0.000000" y="0.333333" />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="u2" kind="input">
--- 						<ssc:Real />
--- 						<ssd:ConnectorGeometry x="0.000000" y="0.666667" />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="y" kind="output">
--- 						<ssc:Real />
--- 						<ssd:ConnectorGeometry x="1.000000" y="0.500000" />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="k1" kind="parameter">
--- 						<ssc:Real />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="k2" kind="parameter">
--- 						<ssc:Real />
--- 					</ssd:Connector>
--- 				</ssd:Connectors>
--- 			</ssd:Component>
--- 			<ssd:Component name="Gain" type="application/x-fmu-sharedlibrary" source="resources/0001_Gain.fmu">
--- 				<ssd:Connectors>
--- 					<ssd:Connector name="u" kind="input">
--- 						<ssc:Real />
--- 						<ssd:ConnectorGeometry x="0.000000" y="0.500000" />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="y" kind="output">
--- 						<ssc:Real />
--- 						<ssd:ConnectorGeometry x="1.000000" y="0.500000" />
--- 					</ssd:Connector>
--- 					<ssd:Connector name="k" kind="parameter">
--- 						<ssc:Real />
--- 					</ssd:Connector>
--- 				</ssd:Connectors>
--- 			</ssd:Component>
--- 		</ssd:Elements>
--- 		<ssd:Connections />
--- 	</ssd:System>
--- 	<ssd:DefaultExperiment startTime="0.000000" stopTime="1.000000">
--- 		<ssd:Annotations>
--- 			<ssc:Annotation type="org.openmodelica">
--- 				<oms:SimulationInformation resultFile="test_res.mat" loggingInterval="0.000000" bufferSize="10" signalFilter="" />
--- 			</ssc:Annotation>
--- 		</ssd:Annotations>
--- 	</ssd:DefaultExperiment>
--- </ssd:SystemStructureDescription>
+-- <?xml version="1.0" encoding="UTF-8"?>
+-- <ssv:ParameterSet xmlns:ssc="http://ssp-standard.org/SSP1/SystemStructureCommon" xmlns:ssv="http://ssp-standard.org/SSP1/SystemStructureParameterValues" version="1.0" name="modelDescriptionStartValues">
+-- 	<ssv:Parameters>
+-- 		<ssv:Parameter name="add.u2">
+-- 			<ssv:Real value="0" />
+-- 		</ssv:Parameter>
+-- 		<ssv:Parameter name="add.u1">
+-- 			<ssv:Real value="0" />
+-- 		</ssv:Parameter>
+-- 		<ssv:Parameter name="add.k2">
+-- 			<ssv:Real value="1" />
+-- 		</ssv:Parameter>
+-- 		<ssv:Parameter name="add.k1">
+-- 			<ssv:Real value="1" />
+-- 		</ssv:Parameter>
+-- 		<ssv:Parameter name="Gain.u">
+-- 			<ssv:Real value="0" />
+-- 		</ssv:Parameter>
+-- 		<ssv:Parameter name="Gain.k">
+-- 			<ssv:Real value="1" />
+-- 		</ssv:Parameter>
+-- 	</ssv:Parameters>
+-- </ssv:ParameterSet>
 -- 
 -- endResult
