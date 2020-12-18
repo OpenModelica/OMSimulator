OpenModelica/OMSimulator

commit 6aace9382858f25bb7939645dbcbb05bf86b92a6

Changed Dll search meachanism with Python 3.8 (#898)

src/OMSimulatorPython/OMSimulator.py
@@ -10,7 +10,11 @@ class OMSimulator:
       dirname = os.path.dirname(__file__)
       omslib = os.path.join(dirname, "@OMSIMULATORLIB_DIR_STRING@", "@OMSIMULATORLIB_STRING@")
 
+    if os.name == 'nt': # If on Windows
+      ddlDir = os.add_dll_directory(os.path.dirname(omslib))
     self.obj=ctypes.CDLL(omslib)
+    if os.name == 'nt': # If on Windows
+      close(ddlDir)
 
     ## oms_status_enu_t
     self.status_ok = 0

commit ceb453bd1a7d863c1275abd2f3e4a373f63692cf

Fixing error introduced in 6aace93
  - That's why we need to test on Windows again

src/OMSimulatorPython/OMSimulator.py
@@ -10,11 +10,11 @@ class OMSimulator:
       dirname = os.path.dirname(__file__)
       omslib = os.path.join(dirname, "@OMSIMULATORLIB_DIR_STRING@", "@OMSIMULATORLIB_STRING@")
 
-    if os.name == 'nt': # If on Windows
-      ddlDir = os.add_dll_directory(os.path.dirname(omslib))
+    if os.name == 'nt': # On Windows
+      dllDir = os.add_dll_directory(os.path.dirname(omslib))
     self.obj=ctypes.CDLL(omslib)
-    if os.name == 'nt': # If on Windows
-      close(ddlDir)
+    if os.name == 'nt':
+      dllDir.close()
 
     ## oms_status_enu_t
     self.status_ok = 0