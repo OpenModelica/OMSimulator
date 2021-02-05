78f8db536e3edb3487644e37d80c16f298cb39bf
Lennart Ochel <lennart.ochel@liu.se>
2021-02-05 15:25:56 +0100

    Non-interactive simulation

--- src/OMSimulatorServer/OMSimulatorServer.py
+++ src/OMSimulatorServer/OMSimulatorServer.py
@@ -41,26 +41,64 @@ def receive_and_reply(socket):
   else:
     print(msg)
 
+def mogrify(topic: str, msg: dict):
+  '''Combines a topic identifier and a json representation of a dictionary'''
+  return '%s %s' % (topic, json.dumps(msg))
+
+def pub_msg(socket, topic, msg: dict):
+  if socket:
+    msg_ = mogrify(topic, msg)
+    socket.send_string(msg_)
+
 def _main():
   # parse command-line arguments
   parser = argparse.ArgumentParser(description='OMS-SERVER', allow_abbrev=False)
-  parser.add_argument('--port', default='1234', help='define the port for the req/rep comunication')
+  parser.add_argument('--model', default=None, required=True, help='define the model to simulate')
+  parser.add_argument('--result-file', default=None, help='defines whether and if so to which file the results will be written')
+  parser.add_argument('--port-rep', type=int, default=None, help='define the port for the req/rep comunication')
+  parser.add_argument('--port-pub', type=int, default=None, help='define the port for the pub/sub comunication')
   args = parser.parse_args()
 
-  # bind the socket
+  logging.info('OMS Server {}'.format(__version__))
+  logging.info('ZMQ {}'.format(zmq.zmq_version()))
+
   context = zmq.Context()
 
-  socket = context.socket(zmq.REP)  #pylint: disable=no-member
-  socket.bind('tcp://*:{}'.format(args.port))
-  socket.RCVTIMEO = 1000  #in milliseconds
-  logging.info('Bound to port {}'.format(args.port))
+  # bind the REP socket
+  if args.port_rep:
+    socket_rep = context.socket(zmq.REP)  #pylint: disable=no-member
+    socket_rep.bind('tcp://*:{}'.format(args.port_rep))
+    socket_rep.RCVTIMEO = 1000  #in milliseconds
+    logging.info('REP socket bound to port {}'.format(args.port_rep))
+  else:
+    socket_rep = None
 
-  while socket:
-    try:
-      receive_and_reply(socket)
-    except KeyboardInterrupt:
-      close_socket_gracefully(socket)
-      socket = None
+  # bind the PUB socket
+  if args.port_pub:
+    socket_sub = context.socket(zmq.PUB)  #pylint: disable=no-member
+    socket_sub.bind('tcp://*:{}'.format(args.port_pub))
+    logging.info('PUB socket bound to port {}'.format(args.port_pub))
+  else:
+    socket_sub = None
+
+  oms = OMSimulator.OMSimulator()
+  oms.setTempDirectory("./temp/")
+  model, status = oms.importFile(args.model)
+  if status != 0:
+    logging.error("Couldn't import model {}".format(args.model))
+    return
+
+  if args.result_file:
+    oms.setResultFile(model, args.result_file)
+    logging.info('Result file: {}'.format(args.result_file))
+
+  pub_msg(socket_sub, 'status', {'progress': 0})
+  oms.instantiate(model)
+  oms.initialize(model)
+  oms.simulate(model)
+  oms.terminate(model)
+  oms.delete(model)
+  pub_msg(socket_sub, 'status', {'progress': 100})
 
 if __name__ == '__main__':
   path = "log/"
@@ -78,7 +116,5 @@ if __name__ == '__main__':
   handler = logging.StreamHandler()
   handler.setFormatter(formatter)
   logging.getLogger().addHandler(handler)
-
-  logging.info('OMS Server {}'.format(__version__))
 
   _main()
