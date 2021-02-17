5bf0755b6465e7664052fe2ff17e6d6f0fecc0c0
Lennart Ochel <lennart.ochel@ri.se>
2021-02-17 12:37:44 +0100

    Add log level and command line options to simulation server (#953)

--- src/OMSimulatorPython/NewAPI.py
+++ src/OMSimulatorPython/NewAPI.py
@@ -1,7 +1,7 @@
 from OMSimulator import Model, Scope, Types
 
 
-def newModel(cref):
+def newModel(cref: str):
   status = Scope._capi.newModel(cref)
   if Types.Status(status) == Types.Status.OK:
     Scope._Scope.append(cref)
@@ -9,7 +9,7 @@ def newModel(cref):
     raise Exception('error {}'.format(Types.Status(status)))
   return Model(cref)
 
-def importFile(file):
+def importFile(file: str):
   cref, status = Scope._capi.importFile(file)
   if Types.Status(status) == Types.Status.OK:
     Scope._Scope.append(cref)
@@ -17,7 +17,20 @@ def importFile(file):
     raise Exception('error {}'.format(Types.Status(status)))
   return Model(cref)
 
-def setTempDirectory(dir):
+def setTempDirectory(dir: str):
   status = Scope._capi.setTempDirectory(dir)
+  if Types.Status(status) != Types.Status.OK:
+    raise Exception('error {}'.format(Types.Status(status)))
+
+def setCommandLineOption(cmd: str):
+  status = Scope._capi.setCommandLineOption(cmd)
+  if Types.Status(status) != Types.Status.OK:
+    raise Exception('error {}'.format(Types.Status(status)))
+
+def setLoggingLevel(level: int):
+  if not isinstance(level, int):
+    raise Exception('bad argument: {}'.format(level))
+
+  status = Scope._capi.setLoggingLevel(level)
   if Types.Status(status) != Types.Status.OK:
     raise Exception('error {}'.format(Types.Status(status)))
