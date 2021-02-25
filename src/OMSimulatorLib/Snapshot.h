b2c11c8c5b588e0d335d6dd39d1418a299f33b40
Lennart Ochel <lennart.ochel@liu.se>
2021-02-25 08:54:05 +0100

    Encapsulate the snapshot implementation

---
+++ src/OMSimulatorLib/Snapshot.h
@@ -0,0 +1,66 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Open Source Modelica Consortium (OSMC),
+ * c/o Linköpings universitet, Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3 LICENSE OR
+ * THIS OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.2.
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ * RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GPL VERSION 3,
+ * ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from OSMC, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or
+ * http://www.openmodelica.org, and in the OpenModelica distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+#ifndef _OMS_SNAPSHOT_H_
+#define _OMS_SNAPSHOT_H_
+
+#include "OMSFileSystem.h"
+#include "Types.h"
+
+#include <pugixml.hpp>
+#include <string>
+#include <unordered_map>
+
+namespace oms
+{
+  class Snapshot
+  {
+  public:
+    Snapshot();
+    ~Snapshot();
+
+    oms_status_enu_t importSnapshot(const char* snapshot);
+    oms_status_enu_t importResourcesFile(const filesystem::path& filename, const filesystem::path& root);
+    oms_status_enu_t importResourcesMemory(const filesystem::path& filename, const char* file);
+    oms_status_enu_t getResources(const filesystem::path& filename, pugi::xml_node& node);
+    void debugPrintFile(const filesystem::path& filename);
+
+  private:
+    // stop the compiler generating methods copying the object
+    Snapshot(Snapshot const& copy);            ///< not implemented
+    Snapshot& operator=(Snapshot const& copy); ///< not implemented
+
+  private:
+    std::unordered_map<filesystem::path, pugi::xml_document> resources;
+  };
+}
+
+#endif
