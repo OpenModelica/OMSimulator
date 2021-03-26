4c0a5362d57195915afa95616692d350062978cd
Lennart Ochel <lennart.ochel@ri.se>
2021-03-26 06:30:07 +0100

    Test MSVC

--- testsuite/AircraftVehicleDemonstrator/AircraftVehicleDemonstrator.lua
+++ testsuite/AircraftVehicleDemonstrator/AircraftVehicleDemonstrator.lua
@@ -2,7 +2,7 @@
 -- teardown_command: rm -rf AircraftVehicleDemonstrator_tmp/ AircraftVehicleDemonstrator.log AircraftVehicleDemonstrator*.dot AircraftVehicleDemonstrator_res.mat
 -- linux: no
 -- mingw: yes
--- win: no
+-- win: yes
 
 oms_setCommandLineOption("--suppressPath=true")
 oms_setTempDirectory("./AircraftVehicleDemonstrator_tmp/")
