f5a79739e71f4bfbde773831f1d8954af9e681cd
Lennart Ochel <lennart.ochel@ri.se>
2021-04-13 02:57:33 +0200

    Expected output

--- testsuite/AircraftVehicleDemonstrator/embrace.py
+++ testsuite/AircraftVehicleDemonstrator/embrace.py
@@ -1,9 +1,9 @@
 ## status: correct
-## teardown_command:
+## teardown_command: rm SystemStructure.ssd event.dot init.dot sim_results.mat simulation.dot
 ## linux: yes
-## mingw: yes
-## win: yes
-## mac: yes
+## mingw: no
+## win: no
+## mac: no
 """
 Created on Mon Dec 21 10:30:26 2020
 
@@ -52,7 +52,7 @@ oms.setResultFile("model", "sim_results.mat")
 oms.setStopTime("model", 1200)
 oms.setFixedStepSize("model", 1e-3)
 oms.setLoggingInterval("model", 1)
-oms.exportDependencyGraphs("model.root","init.dot","event.dot","simulation.dot")
+oms.exportDependencyGraphs("model.root", "init.dot", "event.dot", "simulation.dot")
 
 oms.instantiate("model")
 oms.initialize("model")
@@ -236,6 +236,13 @@ oms.delete("model")
 ## error:   [setInteger] Unknown signal "model.root.ECS_HW.pipeB1.looptype"
 ## info:    Result file: sim_results.mat (bufferSize=1)
 ## error:   [getReal] Unknown signal "model.root.ECS_HW.pipeB1.L"
+## info:    Initialize:
+## info:      model.root.ECS_HW.coolinPackAir.looptype:  2
+## info:      model.root.ECS_SW.PID.k                 :  5.0
+## info:      model.root.ECS_SW.PID.k                 :  10.0
+## info:      model.root.ECS_HW.pipeB1.L              :  0.0
+## info:      model.root.ECS_HW.pipeB.L               :  0.976535328081166
+## info:      model.root.ECS_HW.pipeA.L               :  0.976535328081166
 ## info:    0 warnings
 ## info:    166 errors
 ## endResult
