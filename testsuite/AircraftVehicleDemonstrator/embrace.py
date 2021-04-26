d1b71095f473fdacbd7d88653501c9fc68ed99d8
Robert Hällqvist <robha67@liu.se>
2021-04-26 16:07:27 +0200

    Updated embrace use case

--- testsuite/AircraftVehicleDemonstrator/embrace.py
+++ testsuite/AircraftVehicleDemonstrator/embrace.py
@@ -4,6 +4,7 @@
 ## mingw: no
 ## win: no
 ## mac: no
+
 """
 Created on Mon Dec 21 10:30:26 2020
 
@@ -14,12 +15,12 @@ import OMSimulator
 #OMSimulator.OMSimulator()
 oms=OMSimulator.OMSimulator()
 a=oms.getVersion()
-#print(a)
-oms.setCommandLineOption("--suppressPath=true  --wallTime=true")
+print(a)
+oms.setCommandLineOption("--suppressPath=true  --wallTime=true --progressBar=true")
 
 
-oms.importFile("../resources/embrace.ssp")
-oms.addSubModel("model.root.BC", "../resources/CONOPS.csv")
+oms.importFile("Embrace.ssp")
+oms.addSubModel("model.root.BC", "CONOPS.csv")
 
 ##----------------- Connections to BC files ----------------------------
 #BC -> Consumer
@@ -32,7 +33,7 @@ oms.addConnection("model.root.ECS_HW.Alt","model.root.BC.Alt")
 oms.addConnection("model.root.ECS_HW.Mach","model.root.BC.Mach")
 
 # #BC -> ECS_SW
-oms.addConnection("model.root.ECS_SW.WGT_ON_WHL","model.root.BC.WGT_ON_WHL")
+oms.addConnection("model.root.BC.Aircraft_state", "model.root.AdaptionUnit.Aircraft_State")
 
 # #BC -> Atmos
 oms.addConnection("model.root.Atmos.Alt","model.root.BC.Alt")
@@ -40,27 +41,25 @@ oms.addConnection("model.root.Atmos.Ma","model.root.BC.Mach")
 # #-----------------------------------------------------------------------
 
 #set params
-oms.setReal("model.root.ECS_SW.PID.k", 10)
 oms.setReal("model.root.ECS_HW.eCS.MaxCoolPower.k", 5)
 oms.setInteger("model.root.ECS_HW.coolinPackAir.looptype", 2)
 oms.setInteger("model.root.ECS_HW.phSrc.looptype", 2)
-oms.setInteger("model.root.ECS_HW.pipeB1.looptype", 2)
+oms.setInteger("model.root.ECS_HW.pipeB.looptype", 2)
+oms.setInteger("model.root.ECS_HW.pipeA.looptype", 2)
 
 # #simulation settings
-#oms.setResultFile("model", "ReferenceResults_L=5_CONOPS.mat")
 oms.setResultFile("model", "sim_results.mat")
 oms.setStopTime("model", 1200)
 oms.setFixedStepSize("model", 1e-3)
 oms.setLoggingInterval("model", 1)
-oms.exportDependencyGraphs("model.root", "init.dot", "event.dot", "simulation.dot")
+oms.exportDependencyGraphs("model.root","init.dot","event.dot","simulation.dot")
 
 oms.instantiate("model")
 oms.initialize("model")
 print("info:    Initialize:")
 print("info:      model.root.ECS_HW.coolinPackAir.looptype: " , oms.getInteger("model.root.ECS_HW.coolinPackAir.looptype")[0])
-print("info:      model.root.ECS_SW.PID.k                 : " , oms.getReal("model.root.ECS_HW.eCS.MaxCoolPower.k")[0])
-print("info:      model.root.ECS_SW.PID.k                 : " , oms.getReal("model.root.ECS_SW.PID.k")[0])
-print("info:      model.root.ECS_HW.pipeB1.L              : " , oms.getReal("model.root.ECS_HW.pipeB1.L")[0])
+print("info:      model.root.ECS_HW.eCS.MaxCoolPower.k    : " , oms.getReal("model.root.ECS_HW.eCS.MaxCoolPower.k")[0])
+print("info:      model.root.ECS_HW.pipeC.L               : " , oms.getReal("model.root.ECS_HW.pipeC.L")[0])
 print("info:      model.root.ECS_HW.pipeB.L               : " , oms.getReal("model.root.ECS_HW.pipeB.L")[0])
 print("info:      model.root.ECS_HW.pipeA.L               : " , oms.getReal("model.root.ECS_HW.pipeA.L")[0])
 
