cab626e22a83aff4dc4c8509c767e6b753fdfdbc
Lennart Ochel <lennart.ochel@ri.se>
2021-05-23 16:39:55 +0200

    Add setLoggingInterval to new python interface (#1029)

--- src/OMSimulatorPython/NewAPI.py
+++ src/OMSimulatorPython/NewAPI.py
@@ -1,7 +1,7 @@
 from OMSimulator import Model, Scope, Types
 
 
-def newModel(cref: str):
+def newModel(cref: str) -> Model:
   status = Scope._capi.newModel(cref)
   if Types.Status(status) == Types.Status.OK:
     Scope._Scope.append(cref)
@@ -9,7 +9,7 @@ def newModel(cref: str):
     raise Exception('error {}'.format(Types.Status(status)))
   return Model(cref)
 
-def importFile(file: str):
+def importFile(file: str) -> Model:
   cref, status = Scope._capi.importFile(file)
   if Types.Status(status) == Types.Status.OK:
     Scope._Scope.append(cref)
@@ -17,17 +17,17 @@ def importFile(file: str):
     raise Exception('error {}'.format(Types.Status(status)))
   return Model(cref)
 
-def setTempDirectory(dir: str):
+def setTempDirectory(dir: str) -> None:
   status = Scope._capi.setTempDirectory(dir)
   if Types.Status(status) != Types.Status.OK:
     raise Exception('error {}'.format(Types.Status(status)))
 
-def setCommandLineOption(cmd: str):
+def setCommandLineOption(cmd: str) -> None:
   status = Scope._capi.setCommandLineOption(cmd)
   if Types.Status(status) != Types.Status.OK:
     raise Exception('error {}'.format(Types.Status(status)))
 
-def setLoggingLevel(level: int):
+def setLoggingLevel(level: int) -> None:
   if not isinstance(level, int):
     raise Exception('bad argument: {}'.format(level))
   status = Scope._capi.setLoggingLevel(level)
