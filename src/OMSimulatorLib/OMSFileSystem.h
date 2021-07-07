c9e63bb3c4d6321f26e9a90ad09b600692d080a7
Lennart Ochel <lennart.ochel@ri.se>
2021-07-07 09:03:01 +0200

    Cleanup the filesystem package

--- src/OMSimulatorLib/OMSFileSystem.h
+++ src/OMSimulatorLib/OMSFileSystem.h
@@ -52,11 +52,11 @@ namespace filesystem = std::experimental::filesystem::v1;
 namespace filesystem = boost::filesystem;
 #endif
 
-filesystem::path oms_temp_directory_path(void);
-filesystem::path oms_canonical(filesystem::path p);
-void oms_copy_file(const filesystem::path& from, const filesystem::path& to);
-filesystem::path oms_unique_path(const std::string& prefix);
 filesystem::path naive_uncomplete(const filesystem::path& path, const filesystem::path& base);
+filesystem::path oms_unique_path(const std::string& prefix);
+void oms_copy_file(const filesystem::path& from, const filesystem::path& to);
+filesystem::path oms_canonical(const filesystem::path& p);
 filesystem::path oms_absolute(const filesystem::path& p);
+filesystem::path oms_temp_directory_path(void);
 
 #endif
