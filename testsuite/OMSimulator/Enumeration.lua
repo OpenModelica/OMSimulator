cc14207747c0e55282962d5cc4f9b9314668c08a
Lennart Ochel <lennart.ochel@ri.se>
2021-07-08 09:39:56 +0200

    Make res files unique

--- testsuite/OMSimulator/Enumeration.lua
+++ testsuite/OMSimulator/Enumeration.lua
@@ -8,49 +8,47 @@
 oms_setCommandLineOption("--suppressPath=true")
 oms_setTempDirectory("./Enumeration_lua/")
 
-oms_newModel("test")
-oms_addSystem("test.root", oms_system_sc)
-oms_addSubModel("test.root.A", "../resources/Int1.fmu")
-oms_addSubModel("test.root.B", "../resources/Enum1.fmu")
+oms_newModel("Enumeration")
+oms_addSystem("Enumeration.root", oms_system_sc)
+oms_addSubModel("Enumeration.root.A", "../resources/Int1.fmu")
+oms_addSubModel("Enumeration.root.B", "../resources/Enum1.fmu")
 
 -- integer to enumeration
-oms_addConnection("test.root.A.y", "test.root.B.u")
+oms_addConnection("Enumeration.root.A.y", "Enumeration.root.B.u")
 
-oms_setResultFile("test", "test_res.mat")
+-- oms_exportDependencyGraphs("Enumeration.root", "Enumeration.root_init.dot", "Enumeration.root_sim.dot", "Enumeration.root_continuous.dot")
+-- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" Enumeration.root_init.dot | dot -Tpdf -o Enumeration.root_init.pdf")
+-- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" Enumeration.root_sim.dot | dot -Tpdf -o Enumeration.root_sim.pdf")
+-- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" Enumeration.root_continuous.dot | dot -Tpdf -o Enumeration.root_continuous.pdf")
 
--- oms_exportDependencyGraphs("test.root", "test.root_init.dot", "test.root_sim.dot", "test.root_continuous.dot")
--- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" test.root_init.dot | dot -Tpdf -o test.root_init.pdf")
--- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" test.root_sim.dot | dot -Tpdf -o test.root_sim.pdf")
--- os.execute("gvpr -c \"N[$.degree==0]{delete(root, $)}\" test.root_continuous.dot | dot -Tpdf -o test.root_continuous.pdf")
-
-oms_instantiate("test")
+oms_instantiate("Enumeration")
 print("info:    Instantiation")
-print("info:      test.root.A.y     : " .. oms_getInteger("test.root.A.y"))
-print("info:      test.root.B.u     : " .. oms_getInteger("test.root.B.u"))
+print("info:      Enumeration.root.A.y     : " .. oms_getInteger("Enumeration.root.A.y"))
+print("info:      Enumeration.root.B.u     : " .. oms_getInteger("Enumeration.root.B.u"))
 
-oms_initialize("test")
+oms_initialize("Enumeration")
 print("info:    Initialization")
-print("info:      test.root.A.y     : " .. oms_getInteger("test.root.A.y"))
-print("info:      test.root.B.u     : " .. oms_getInteger("test.root.B.u"))
+print("info:      Enumeration.root.A.y     : " .. oms_getInteger("Enumeration.root.A.y"))
+print("info:      Enumeration.root.B.u     : " .. oms_getInteger("Enumeration.root.B.u"))
 
-oms_simulate("test")
+oms_simulate("Enumeration")
 print("info:    Simulation")
-print("info:      test.root.A.y     : " .. oms_getInteger("test.root.A.y"))
-print("info:      test.root.B.u     : " .. oms_getInteger("test.root.B.u"))
+print("info:      Enumeration.root.A.y     : " .. oms_getInteger("Enumeration.root.A.y"))
+print("info:      Enumeration.root.B.u     : " .. oms_getInteger("Enumeration.root.B.u"))
 
-oms_terminate("test")
-oms_delete("test")
+oms_terminate("Enumeration")
+oms_delete("Enumeration")
 
 -- Result:
 -- info:    model doesn't contain any continuous state
 -- info:    Instantiation
--- info:      test.root.A.y     : 2
--- info:      test.root.B.u     : 0
--- info:    Result file: test_res.mat (bufferSize=1)
+-- info:      Enumeration.root.A.y     : 2
+-- info:      Enumeration.root.B.u     : 0
+-- info:    Result file: Enumeration_res.mat (bufferSize=10)
 -- info:    Initialization
--- info:      test.root.A.y     : 2
--- info:      test.root.B.u     : 2
+-- info:      Enumeration.root.A.y     : 2
+-- info:      Enumeration.root.B.u     : 2
 -- info:    Simulation
--- info:      test.root.A.y     : 2
--- info:      test.root.B.u     : 2
+-- info:      Enumeration.root.A.y     : 2
+-- info:      Enumeration.root.B.u     : 2
 -- endResult
--- testsuite/OMSimulator/simulation.lua
+++ testsuite/OMSimulator/simulation.lua
@@ -1,5 +1,5 @@
 -- status: correct
--- teardown_command: rm -rf simulation-lua/ test_init2.dot test_event2.dot test_sim2.dot test_res.mat
+-- teardown_command: rm -rf simulation-lua/ simulation_init.dot simulation_event.dot simulation_sim.dot simulation_res.mat
 -- linux: yes
 -- mingw32: no
 -- mingw64: no
@@ -27,52 +27,52 @@ function printStatus(status, expected)
   print("status:  [" .. cmp .. "] " .. status)
 end
 
-status = oms_newModel("test")
+status = oms_newModel("simulation")
 printStatus(status, 0)
 
-status = oms_addSystem("test.co_sim", oms_system_wc)
+status = oms_addSystem("simulation.co_sim", oms_system_wc)
 printStatus(status, 0)
 
-status = oms_addSubModel("test.co_sim.A", "../resources/tlm.source.fmu")
+status = oms_addSubModel("simulation.co_sim.A", "../resources/tlm.source.fmu")
 printStatus(status, 0)
 
-status = oms_addSubModel("test.co_sim.B", "../resources/tlm.source.fmu")
+status = oms_addSubModel("simulation.co_sim.B", "../resources/tlm.source.fmu")
 printStatus(status, 0)
 
-oms_exportDependencyGraphs("test.co_sim", "test_init2.dot", "test_event2.dot", "test_sim2.dot")
+oms_exportDependencyGraphs("simulation.co_sim", "simulation_init.dot", "simulation_event.dot", "simulation_sim.dot")
 
-status = oms_instantiate("test")
+status = oms_instantiate("simulation")
 printStatus(status, 0)
 
-v, status = oms_getReal("test.co_sim.A.A")
+v, status = oms_getReal("simulation.co_sim.A.A")
 printStatus(status, 0)
-print("test.co_sim.A.A: " .. v)
+print("simulation.co_sim.A.A: " .. v)
 
-status = oms_setReal("test.co_sim.A.A", v + 1.0)
-printStatus(status, 0)
-
-v, status = oms_getReal("test.co_sim.A.A")
-printStatus(status, 0)
-print("test.co_sim.A.A: " .. v)
-
-status = oms_initialize("test")
+status = oms_setReal("simulation.co_sim.A.A", v + 1.0)
 printStatus(status, 0)
 
-v, status = oms_getReal("test.co_sim.A.y")
+v, status = oms_getReal("simulation.co_sim.A.A")
 printStatus(status, 0)
-print("test.co_sim.A.y: " .. v)
+print("simulation.co_sim.A.A: " .. v)
 
-status = oms_simulate("test")
-printStatus(status, 0)
-
-v, status = oms_getReal("test.co_sim.A.y")
-printStatus(status, 0)
-print("test.co_sim.A.y: " .. v)
-
-status = oms_terminate("test")
+status = oms_initialize("simulation")
 printStatus(status, 0)
 
-status = oms_delete("test")
+v, status = oms_getReal("simulation.co_sim.A.y")
+printStatus(status, 0)
+print("simulation.co_sim.A.y: " .. v)
+
+status = oms_simulate("simulation")
+printStatus(status, 0)
+
+v, status = oms_getReal("simulation.co_sim.A.y")
+printStatus(status, 0)
+print("simulation.co_sim.A.y: " .. v)
+
+status = oms_terminate("simulation")
+printStatus(status, 0)
+
+status = oms_delete("simulation")
 printStatus(status, 0)
 
 -- Result:
@@ -86,17 +86,17 @@ printStatus(status, 0)
 -- status:  [correct] ok
 -- status:  [correct] ok
 -- status:  [correct] ok
--- test.co_sim.A.A: 1.0
+-- simulation.co_sim.A.A: 1.0
 -- status:  [correct] ok
 -- status:  [correct] ok
--- test.co_sim.A.A: 2.0
--- info:    Result file: test_res.mat (bufferSize=10)
+-- simulation.co_sim.A.A: 2.0
+-- info:    Result file: simulation_res.mat (bufferSize=10)
 -- status:  [correct] ok
 -- status:  [correct] ok
--- test.co_sim.A.y: 0.0
+-- simulation.co_sim.A.y: 0.0
 -- status:  [correct] ok
 -- status:  [correct] ok
--- test.co_sim.A.y: 1.6829419696158
+-- simulation.co_sim.A.y: 1.6829419696158
 -- status:  [correct] ok
 -- status:  [correct] ok
 -- info:    2 warnings
