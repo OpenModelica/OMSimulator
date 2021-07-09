c5657f5717ddb9a54b10ad4cf961ea53266c4cb4
Lennart Ochel <lennart.ochel@ri.se>
2021-07-09 08:49:53 +0200

    Update SSP file for importStartValues.lua

--- testsuite/OMSimulator/importStartValues.lua
+++ testsuite/OMSimulator/importStartValues.lua
@@ -15,23 +15,10 @@ oms_importFile("../resources/importStartValues.ssp");
 src1 = oms_exportSnapshot("importStartValues")
 print(src1)
 
-oms_setStopTime("importStartValues", 2)
-
-oms_instantiate("importStartValues")
-
-oms_initialize("importStartValues")
-oms_simulate("importStartValues")
-
 oms_terminate("importStartValues")
 oms_delete("importStartValues")
 
-
 -- Result:
--- warning: Wrong/deprecated content detected but successfully loaded. Please re-export the SSP file to avoid this message.
--- warning: Wrong/deprecated content detected but successfully loaded. Please re-export the SSP file to avoid this message.
--- warning: Wrong/deprecated content detected but successfully loaded. Please re-export the SSP file to avoid this message.
--- warning: Wrong/deprecated content detected but successfully loaded. Please re-export the SSP file to avoid this message.
--- warning: Wrong/deprecated content detected but successfully loaded. Please re-export the SSP file to avoid this message.
 -- <?xml version="1.0"?>
 -- <oms:snapshot
 --   xmlns:oms="https://raw.githubusercontent.com/OpenModelica/OMSimulator/master/schema/oms.xsd"
@@ -194,8 +181,4 @@ oms_delete("importStartValues")
 --   </oms:file>
 -- </oms:snapshot>
 --
--- info:    model doesn't contain any continuous state
--- info:    Result file: importStartValues_res.mat (bufferSize=10)
--- info:    5 warnings
--- info:    0 errors
 -- endResult
