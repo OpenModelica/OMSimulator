5c54efc4ba1ae8677c58d6e85811381c2b369ffa
arun3688 <rain100falls@gmail.com>
2021-09-06 15:07:17 +0200

    fix xerces library name

--- config.cmake/FindXERCESLibrary.cmake
+++ config.cmake/FindXERCESLibrary.cmake
@@ -4,20 +4,25 @@
 # maybe make sure the headers exits too.
 
 find_library(XERCESLibrary_LIBRARY
-  NAMES xerces
-  HINTS ${XERCESLibrary_ROOT}
+  NAMES xerces-c
+  HINTS ${XERCESLibrary_ROOT} ${XERCESLibrary_ROOT}/lib
 )
 
-if (XERCESLibrary_ROOT)
+if (XERCESLibrary_LIBRARY)
   message(STATUS "Found XERCES")
   set(XERCESLibrary_INCLUDEDIR ${XERCESLibrary_ROOT}/include)
   set(XERCESLibrary_LIBRARYDIR ${XERCESLibrary_ROOT}/lib)
   message(STATUS "Xerces_ROOT:                " ${XERCESLibrary_ROOT})
+  message(STATUS "XercesLibrary_LIBRARY:      " ${XERCESLibrary_LIBRARY})
   message(STATUS "XercesC_INCLUDE_DIRS:       " ${XERCESLibrary_INCLUDEDIR})
   message(STATUS "XercesC_LIBRARIES:          " ${XERCESLibrary_LIBRARYDIR})
 else()
-  message(STATUS "Unable to find the requested Xerces Library" )
-  message(STATUS "Looked in XERCESLibrary_ROOT ${XERCESLibrary_ROOT}" )
-  message(SEND_ERROR "Could not find xerces. Make sure you have set the XERCESLibrary_ROOT in the CMakeFile.txt to point to your XERCESLibrary installation." )
+  if(XERCESLibrary_FIND_REQUIRED)
+    message(STATUS "Unable to find the requested Xerces Library" )
+    message(STATUS "Looked in XERCESLibrary_ROOT ${XERCESLibrary_ROOT}" )
+    message(SEND_ERROR "Could not find xerces. Make sure you have set the XERCESLibrary_ROOT in the CMakeFile.txt to point to your XERCESLibrary installation." )
+  else()
+    message(STATUS "XERCESLibrary - NOT Found" )
+  endif(XERCESLibrary_FIND_REQUIRED)
 endif()
 
