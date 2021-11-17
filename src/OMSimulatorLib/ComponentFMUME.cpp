1327bde429701174667b2b29d6c2dc5b4a301296
Mahder Gebremedhin <mahder.geb@gmail.com>
2021-11-17 16:36:30 +0100

    Include cmath explicitly.
      - The files use std::isnan and std::isinf from cmath.
      - It breaks on old compilers (e.g. gcc-5) because the header might not
        be included indirectly.

--- src/OMSimulatorLib/ComponentFMUME.cpp
+++ src/OMSimulatorLib/ComponentFMUME.cpp
@@ -44,6 +44,8 @@
 #include <JM/jm_portability.h>
 #include <RegEx.h>
 #include <unordered_set>
+#include <cmath>
+
 
 oms::ComponentFMUME::ComponentFMUME(const ComRef& cref, System* parentSystem, const std::string& fmuPath)
   : oms::Component(cref, oms_component_fmu, parentSystem, fmuPath), fmuInfo(fmuPath)
