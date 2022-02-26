13397c9e0a78d3b33c0283c2ee6c71c6e2ac91b0
Lennart Ochel <lennart.ochel@ri.se>
2022-02-26 09:50:43 +0100

    Implement getString

--- src/OMSimulatorLib/OMSimulator.cpp
+++ src/OMSimulatorLib/OMSimulator.cpp
@@ -40,6 +40,7 @@
 #include "Logging.h"
 #include "MatReader.h"
 #include "Model.h"
+#include "OMSString.h"
 #include "ResultReader.h"
 #include "Scope.h"
 #include "SignalDerivative.h"
@@ -53,15 +54,15 @@
 #endif
 #include "Types.h"
 #include "Version.h"
+
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
 #include <miniunz.h>
+#include <mutex>
 #include <pugixml.hpp>
 #include <string>
-
-#include <iostream>
-#include <chrono>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
 
 #if defined(OMS_STATIC)
 extern "C"
@@ -1530,6 +1531,35 @@ oms_status_enu_t oms_getStopTime(const char* cref, double* stopTime)
     return logError_ModelNotInScope(front);
 
   *stopTime = model->getStopTime();
+  return oms_status_ok;
+}
+
+oms_status_enu_t oms_getString(const char* cref, char** value)
+{
+  if (!value)
+    return logError("Null-pointer passed to oms_getString");
+  *value = nullptr;
+
+  oms::ComRef tail(cref);
+  oms::ComRef front = tail.pop_front();
+
+  oms::Model* model = oms::Scope::GetInstance().getModel(front);
+  if (!model)
+    return logError_ModelNotInScope(front);
+
+  front = tail.pop_front();
+  oms::System* system = model->getSystem(front);
+  if (!system)
+    return logError_SystemNotInModel(model->getCref(), front);
+
+  std::string value_;
+  oms_status_enu_t status = system->getString(tail, value_);
+  if (oms_status_ok != status)
+    return status;
+
+  *value = oms::mallocAndCopyString(value_);
+  if (!*value)
+    return oms_status_fatal;
   return oms_status_ok;
 }
 
--- src/OMSimulatorLib/Values.cpp
+++ src/OMSimulatorLib/Values.cpp
@@ -95,6 +95,18 @@ oms_status_enu_t oms::Values::getReal(const ComRef& cref, double& value)
   return oms_status_error;
 }
 
+oms_status_enu_t oms::Values::getString(const ComRef& cref, std::string& value)
+{
+  auto stringValue = stringStartValues.find(cref);
+  if (stringValue != stringStartValues.end())
+  {
+    value = stringValue->second;
+    return oms_status_ok;
+  }
+
+  return oms_status_error;
+}
+
 oms_status_enu_t oms::Values::getInteger(const ComRef& cref, int& value)
 {
   auto integerValue = integerStartValues.find(cref);
@@ -266,7 +278,7 @@ oms_status_enu_t oms::Values::getIntegerResources(const ComRef& cref, int& value
   {
     for (auto &res: it.allresources)
     {
-      if (externalInput && oms_modelState_simulation == modelState && res.second.integerValues[cref] != 0.0)
+      if (externalInput && oms_modelState_simulation == modelState && res.second.integerValues[cref] != 0)
       {
         if (res.second.linkResources)
         {
@@ -295,7 +307,7 @@ oms_status_enu_t oms::Values::getBooleanResources(const ComRef& cref, bool& valu
   {
     for (auto &res: it.allresources)
     {
-      if (externalInput && oms_modelState_simulation == modelState && res.second.booleanValues[cref] != 0.0)
+      if (externalInput && oms_modelState_simulation == modelState && res.second.booleanValues[cref] != false)
       {
         if (res.second.linkResources)
         {
@@ -309,6 +321,35 @@ oms_status_enu_t oms::Values::getBooleanResources(const ComRef& cref, bool& valu
         if (res.second.linkResources)
         {
           value = booleanValue->second;
+          return oms_status_ok;
+        }
+      }
+    }
+  }
+
+  return oms_status_error;
+}
+
+oms_status_enu_t oms::Values::getStringResources(const ComRef& cref, std::string& value, bool externalInput, oms_modelState_enu_t modelState)
+{
+  for (auto &it: parameterResources)
+  {
+    for (auto &res: it.allresources)
+    {
+      if (externalInput && oms_modelState_simulation == modelState && res.second.stringValues[cref] != "")
+      {
+        if (res.second.linkResources)
+        {
+          value = res.second.stringValues[cref];
+          return oms_status_ok;
+        }
+      }
+      auto stringValue = res.second.stringStartValues.find(cref);
+      if (stringValue != res.second.stringStartValues.end())
+      {
+        if (res.second.linkResources)
+        {
+          value = stringValue->second;
           return oms_status_ok;
         }
       }
