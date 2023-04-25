da8afdcacc7ff8eff4a894231a7c27b3d7103420
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2023-04-25 16:26:25 +0200

    Exclude rst files from api

--- doc/UsersGuide/source/conf.py
+++ doc/UsersGuide/source/conf.py
@@ -77,7 +77,7 @@ open("releaselink.inc", "w").write("Version: %s" % version)
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ["api"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
