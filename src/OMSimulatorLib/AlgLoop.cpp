d552d7873c287fb1eafd778e96b5d273d22838fd
Reino Ruusu <reino.ruusu@semantum.fi>
2024-12-04 11:48:52 +0200

    Improve debugging output from KINSol
    - Report tolerance target at start of solution
    - Show more decimals than one
    - Use a one-based count for the loops

--- src/OMSimulatorLib/AlgLoop.cpp
+++ src/OMSimulatorLib/AlgLoop.cpp
@@ -391,6 +391,12 @@ oms::KinsolSolver* oms::KinsolSolver::NewKinsolSolver(const int algLoopNum, cons
   return kinsolSolver;
 }
 
+static std::string to_string(double value) {
+  std::ostringstream s;
+  s << value;
+  return s.str();
+}
+
 /**
  * @brief Solve algebraic system with KINSOL
  *
@@ -413,7 +419,10 @@ oms_status_enu_t oms::KinsolSolver::kinsolSolve(System& syst, DirectedGraph& gra
   int flag;
   double fNormValue;
 
-  logDebug("Solving system " + std::to_string(kinsolUserData->algLoopNumber));
+  if (Flags::DumpAlgLoops())
+    logInfo("Solving system " + std::to_string(kinsolUserData->algLoopNumber + 1) + " to within tolerance " + to_string(fnormtol));
+  else
+    logDebug("Solving system " + std::to_string(kinsolUserData->algLoopNumber + 1) + " to within tolerance " + to_string(fnormtol));
 
   if (SCC.connections.size() != size)
   {
@@ -448,8 +457,8 @@ oms_status_enu_t oms::KinsolSolver::kinsolSolve(System& syst, DirectedGraph& gra
   fNormValue = N_VWL2Norm(fTmp, fScale);
   if ( fNormValue > fnormtol )
   {
-    logWarning("Solution of algebraic loop " + std::to_string(((KINSOL_USER_DATA *)user_data)->algLoopNumber) + "not within precission given by fnormtol: " + std::to_string(fnormtol));
-    logDebug("2-norm of residual of solution: " + std::to_string(fNormValue));
+    logWarning("Solution of algebraic loop " + std::to_string(((KINSOL_USER_DATA *)user_data)->algLoopNumber + 1) + " not within precission given by fnormtol: " + to_string(fnormtol));
+    logDebug("2-norm of residual of solution: " + to_string(fNormValue));
     return oms_status_warning;
   }
 
@@ -649,7 +658,7 @@ oms_status_enu_t oms::AlgLoop::fixPointIteration(System& syst, DirectedGraph& gr
   {
     return logError("max. number of iterations (" + std::to_string(maxIterations) + ") exceeded at time = " + std::to_string(syst.getTime()));
   }
-  logDebug("CompositeModel::solveAlgLoop: maxRes: " + std::to_string(maxRes) + ", iterations: " + std::to_string(it) + " at time = " + std::to_string(syst.getTime()));
+  logDebug("CompositeModel::solveAlgLoop: maxRes: " + to_string(maxRes) + ", iterations: " + std::to_string(it) + " at time = " + std::to_string(syst.getTime()));
   return oms_status_ok;
 }
 
