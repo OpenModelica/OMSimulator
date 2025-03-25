8b42b8f0cc824ba99eeebd9489858b319188d52d
Lennart Ochel <lennart.ochel@vti.se>
2025-03-25 15:13:34 +0100

    Add test for CRef

---
+++ testsuite/api/cref.py
@@ -0,0 +1,24 @@
+## status: correct
+## teardown_command: rm -rf NewSSP3.ssp tmp-NewSSP3/
+## linux: yes
+## mingw32: yes
+## mingw64: yes
+## win: yes
+## mac: yes
+
+from OMSimulator import CRef
+
+x = CRef('foo', 'bar')
+y = CRef('foo', 'bar', 'baz')
+z = CRef(y)
+print(x, y, z)
+
+x.names[0] = 'x'
+y.names[0] = 'y'
+z.names[0] = 'z'
+print(x, y, z)
+
+## Result:
+## foo/bar foo/bar/baz foo/bar/baz
+## x/bar y/bar/baz z/bar/baz
+## endResult
