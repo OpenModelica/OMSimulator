a5186c0fbb5cdd57b704ffdd4012d2ea773730b2
Lennart Ochel <lennart.ochel@outlook.com>
2025-03-27 16:17:49 +0100

    Improve suppressing paths (#1443)

--- testsuite/api/NewSSP.py
+++ testsuite/api/NewSSP.py
@@ -6,6 +6,9 @@
 ## win: yes
 ## mac: yes
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 from OMSimulator import SSD, SSP, Settings
 
 Settings.suppressPath = True
@@ -27,18 +30,23 @@ model2 = SSP('NewSSP.ssp', temp_dir="./tmp-NewSSP/model2/")
 model2.list()
 
 ## Result:
+## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
+## INFO:OMSimulator.ssd:SSD 'default' exported to <hidden>
+## INFO:OMSimulator.ssd:SSD 'Variant-B' exported to <hidden>
+## INFO:OMSimulator.ssp:SSP file 'NewSSP.ssp' successfully exported!
+## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
 ## <class 'OMSimulator.ssp.SSP'>
 ## |-- Resources:
 ## |--   resources/Modelica.Blocks.Math.Add.fmu
 ## |-- <class 'OMSimulator.ssd.SSD'>
-## |-- Inactive variant "default": <hidden>
+## |-- Inactive variant "default": None
 ## |--   System: default
 ## |--   Connectors:
 ## |--   DefaultExperiment
 ## |--     startTime: 0.0
 ## |--     stopTime: 1.0
 ## |-- <class 'OMSimulator.ssd.SSD'>
-## |-- Active variant "Variant-B": <hidden>
+## |-- Active variant "Variant-B": None
 ## |--   System: Variant-B
 ## |--   Connectors:
 ## |--   DefaultExperiment
@@ -61,4 +69,6 @@ model2.list()
 ## |--   DefaultExperiment
 ## |--     startTime: 0.0
 ## |--     stopTime: 1.0
+## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
+## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
 ## endResult
