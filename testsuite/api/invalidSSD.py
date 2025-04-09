13d942148901f42202717a203b30202777c6aa5e
arun3688 <rain100falls@gmail.com>
2025-04-09 13:54:49 +0200

    expected output

--- testsuite/api/invalidSSD.py
+++ testsuite/api/invalidSSD.py
@@ -16,24 +16,14 @@ from OMSimulator import SSD, SSP, Settings
 
 Settings.suppressPath = True
 
-model = SSP('../resources/invalidSSD.ssp')
+try :
+  model = SSP('../resources/invalidSSD.ssp')
+except Exception as e:
+  print(f"Error: {e}")
 
 ## Result:
 ## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
-## Traceback (most recent call last):
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/testsuite/api/invalidSSD.py", line 19, in <module>
-##     model = SSP('../resources/invalidSSD.ssp', temp_dir="./invalid-ssd/")
-##             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 30, in __init__
-##     self._extract_ssp(path)
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 67, in _extract_ssp
-##     ssd = SSD.importFromFile(ssd_path, self.resources)
-##           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssd.py", line 39, in importFromFile
-##     utils.validateSSP(root, filename, "SystemStructureDescription.xsd")
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 122, in validateSSP
-##     raise Exception(message)
-## Exception: Failed to validate SystemStructure.ssd against schemafile SystemStructureDescription.xsd
-## ERROR (line 16, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureDescription}Connector', attribute 'type': The attribute 'type' is not allowed.
 ## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
+## Error: Failed to validate SystemStructure.ssd against schemafile SystemStructureDescription.xsd
+## ERROR (line 16, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureDescription}Connector', attribute 'type': The attribute 'type' is not allowed.
 ## endResult
--- testsuite/api/invalidSSV.py
+++ testsuite/api/invalidSSV.py
@@ -16,31 +16,14 @@ from OMSimulator import SSD, SSP, Settings
 
 Settings.suppressPath = True
 
-model = SSP('../resources/invalidSSV.ssp')
+try:
+  model = SSP('../resources/invalidSSV.ssp')
+except Exception as e:
+  print(f"Error: {e}")
 
 ## Result:
 ## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
-## Traceback (most recent call last):
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/testsuite/api/invalidSSV.py", line 19, in <module>
-##     model = SSP('../resources/invalidSSV.ssp', temp_dir="./invalid-ssv/")
-##             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 30, in __init__
-##     self._extract_ssp(path)
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 60, in _extract_ssp
-##     self._addResource(path, path.relative_to(self.temp_dir), copy=False)
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 107, in _addResource
-##     self.resources[str(new_name)] = SSV(ssv_path = filePath)
-##                                     ^^^^^^^^^^^^^^^^^^^^^^^^
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssv.py", line 15, in __init__
-##     self.importFromSSV(self.filename)
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssv.py", line 42, in importFromSSV
-##     parameterValues = utils.parseSSV(filename)
-##                       ^^^^^^^^^^^^^^^^^^^^^^^^
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 90, in parseSSV
-##     validateSSP(root, filename, "SystemStructureParameterValues.xsd")
-##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 122, in validateSSP
-##     raise Exception(message)
-## Exception: Failed to validate System1.ssv against schemafile SystemStructureParameterValues.xsd
-## ERROR (line 4, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureParameterValues}Parameter', attribute 'type': The attribute 'type' is not allowed.
 ## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
+## Error: Failed to validate System1.ssv against schemafile SystemStructureParameterValues.xsd
+## ERROR (line 4, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureParameterValues}Parameter', attribute 'type': The attribute 'type' is not allowed.
 ## endResult
