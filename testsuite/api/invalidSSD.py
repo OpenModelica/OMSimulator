4b67d344666f6aa7e2e5d693f8f1b1b750fe7d10
arun3688 <rain100falls@gmail.com>
2025-04-09 13:26:01 +0200

    add tests to validate invalid ssd and ssv

---
+++ testsuite/api/invalidSSD.py
@@ -0,0 +1,39 @@
+## status: correct
+## teardown_command: rm -rf invalid-ssd/
+## linux: yes
+## ucrt64: yes
+## win: yes
+## mac: yes
+
+## This example demonstrates how to export an SSD file from an SSP file.
+## the main purpose of this example is to parse all the elements in the SSD file
+## and export it to a file.
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from OMSimulator import SSD, SSP, Settings
+
+Settings.suppressPath = True
+
+model = SSP('../resources/invalidSSD.ssp', temp_dir="./invalid-ssd/")
+
+## Result:
+## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
+## Traceback (most recent call last):
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/testsuite/api/invalidSSD.py", line 19, in <module>
+##     model = SSP('../resources/invalidSSD.ssp', temp_dir="./invalid-ssd/")
+##             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 30, in __init__
+##     self._extract_ssp(path)
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 67, in _extract_ssp
+##     ssd = SSD.importFromFile(ssd_path, self.resources)
+##           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssd.py", line 39, in importFromFile
+##     utils.validateSSP(root, filename, "SystemStructureDescription.xsd")
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 122, in validateSSP
+##     raise Exception(message)
+## Exception: Failed to validate SystemStructure.ssd against schemafile SystemStructureDescription.xsd
+## ERROR (line 16, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureDescription}Connector', attribute 'type': The attribute 'type' is not allowed.
+## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
+## endResult
---
+++ testsuite/api/invalidSSV.py
@@ -0,0 +1,46 @@
+## status: correct
+## teardown_command: rm -rf invalid-ssv/
+## linux: yes
+## ucrt64: yes
+## win: yes
+## mac: yes
+
+## This example demonstrates how to export an SSD file from an SSP file.
+## the main purpose of this example is to parse all the elements in the SSD file
+## and export it to a file.
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from OMSimulator import SSD, SSP, Settings
+
+Settings.suppressPath = True
+
+model = SSP('../resources/invalidSSV.ssp', temp_dir="./invalid-ssv/")
+
+## Result:
+## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
+## Traceback (most recent call last):
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/testsuite/api/invalidSSV.py", line 19, in <module>
+##     model = SSP('../resources/invalidSSV.ssp', temp_dir="./invalid-ssv/")
+##             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 30, in __init__
+##     self._extract_ssp(path)
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 60, in _extract_ssp
+##     self._addResource(path, path.relative_to(self.temp_dir), copy=False)
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssp.py", line 107, in _addResource
+##     self.resources[str(new_name)] = SSV(ssv_path = filePath)
+##                                     ^^^^^^^^^^^^^^^^^^^^^^^^
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssv.py", line 15, in __init__
+##     self.importFromSSV(self.filename)
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/ssv.py", line 42, in importFromSSV
+##     parameterValues = utils.parseSSV(filename)
+##                       ^^^^^^^^^^^^^^^^^^^^^^^^
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 90, in parseSSV
+##     validateSSP(root, filename, "SystemStructureParameterValues.xsd")
+##   File "C:/OPENMODELICAGIT/OpenModelica/OMSimulator/install/lib/OMSimulator/utils.py", line 122, in validateSSP
+##     raise Exception(message)
+## Exception: Failed to validate System1.ssv against schemafile SystemStructureParameterValues.xsd
+## ERROR (line 4, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureParameterValues}Parameter', attribute 'type': The attribute 'type' is not allowed.
+## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
+## endResult
