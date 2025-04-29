7b1e8c9cef3e8317560f70abae78f1ca5189c5f0
Lennart Ochel <lennart.ochel@vti.se>
2025-04-29 11:16:26 +0200

    Add instantiated_model.py

--- src/OMSimulatorPython/__init__.py
+++ src/OMSimulatorPython/__init__.py
@@ -15,9 +15,25 @@ from OMSimulator.ssp import SSP
 from OMSimulator.ssv import SSV
 from OMSimulator.system import System
 from OMSimulator.variable import Causality, SignalType, Variable
+from OMSimulator.instantiated_model import InstantiatedModel
 
 # Define public API
-__all__ = ['Capi', 'Connection', 'Connector', 'CRef', 'FMU', 'Settings', 'SSD', 'SSP', 'SSV', 'System', 'Causality', 'SignalType', 'Variable']
+__all__ = [
+    'Capi',
+    'Causality',
+    'Connection',
+    'Connector',
+    'CRef',
+    'FMU',
+    'InstantiatedModel',
+    'Settings',
+    'SignalType',
+    'SSD',
+    'SSP',
+    'SSV',
+    'System',
+    'Variable',
+  ]
 
 __version__ = '@OMS_SHORT_VERSION_STRING@'
 __author__ = 'Open Source Modelica Consortium (OSMC)'
---
+++ src/OMSimulatorPython/instantiated_model.py
@@ -0,0 +1,19 @@
+
+
+
+
+class InstantiatedModel:
+  def __init__(self, json_description):
+    pass
+
+  def initialize(self):
+    pass
+
+  def simulate(self):
+    pass
+
+  def setValues(self):
+    pass
+
+  def getValues(self):
+    pass
