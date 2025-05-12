390e1192221545a4883870068bd7eda4b18e53da
arun3688 <rain100falls@gmail.com>
2025-05-13 00:15:20 +0200

    expected output

--- testsuite/api/setValueSSV3.py
+++ testsuite/api/setValueSSV3.py
@@ -1,5 +1,5 @@
 ## status: correct
-## teardown_command: rm -rf setValueSSV3.ssp tmp-setValueSSV3/ myfile3.ssv myfile4.ssv myfile5.ssv
+## teardown_command: rm -rf setValueSSV3.ssp tmp-setValueSSV3/ myfile5.ssv myfile6.ssv myfile7.ssv
 ## linux: yes
 ## ucrt64: yes
 ## win: yes
@@ -32,37 +32,37 @@ component2 = model.addComponent(CRef('default', 'Add2'), 'resources/Add.fmu')
 ssv1 = SSV()
 ssv1.setValue("param1", 2.0)
 ssv1.setValue("input1", 3.0)
-ssv1.export("myfile3.ssv")
+ssv1.export("myfile5.ssv")
 
 ssv2 = SSV()
 ssv2.setValue("k1", 2.0, "m")
 ssv2.setValue("k2", 3.0, "kg")
 ssv2.setValue("param3", "hello")
-ssv2.export("myfile4.ssv")
+ssv2.export("myfile6.ssv")
 
 ssv3 = SSV()
 ssv3.setValue("k1", 200.0, "m")
 ssv3.setValue("k2", 300.0, "kg")
 ssv3.setValue("param3", "ssp")
-ssv3.export("myfile5.ssv")
+ssv3.export("myfile7.ssv")
 
 
 ## add myfile3.ssv to to ssp resources
-model.addResource("myfile3.ssv", "resources/myfile3.ssv")
+model.addResource("myfile5.ssv", "resources/myfile5.ssv")
 ## add myfile4.ssv to to ssp resources
-model.addResource("myfile4.ssv", "resources/myfile4.ssv")
+model.addResource("myfile6.ssv", "resources/myfile6.ssv")
 ## add myfile5.ssv to to ssp resources
-model.addResource("myfile4.ssv", "resources/myfile5.ssv")
+model.addResource("myfile7.ssv", "resources/myfile7.ssv")
 
-model.addSSV(CRef('default'), 'resources/myfile3.ssv')
-model.addSSV(CRef('default'), 'resources/myfile4.ssv')
+model.addSSV(CRef('default'), 'resources/myfile5.ssv')
+model.addSSV(CRef('default'), 'resources/myfile6.ssv')
 
-model.addSSV(CRef('default', 'sub-system'), 'resources/myfile4.ssv')
+model.addSSV(CRef('default', 'sub-system'), 'resources/myfile6.ssv')
 
 ## reference myfile1.ssv to Add1
-model.addSSV(CRef('default', 'Add1'), 'resources/myfile4.ssv')
+model.addSSV(CRef('default', 'Add1'), 'resources/myfile6.ssv')
 ## reference myfile1.ssv to Add1
-model.addSSV(CRef('default', 'Add1'), 'resources/myfile5.ssv')
+model.addSSV(CRef('default', 'Add1'), 'resources/myfile7.ssv')
 
 model.export('setValueSSV3.ssp')
 
@@ -74,34 +74,34 @@ model2.list()
 ## <class 'OMSimulator.ssp.SSP'>
 ## |-- Resources:
 ## |--   resources/Add.fmu
-## |--   resources/myfile3.ssv
+## |--   resources/myfile5.ssv
 ## |--   |-- Parameter Bindings:
 ## |--   |-- |-- (Real param1, 2.0, None, 'None')
 ## |--   |-- |-- (Real input1, 3.0, None, 'None')
-## |--   resources/myfile4.ssv
+## |--   resources/myfile6.ssv
 ## |--   |-- Parameter Bindings:
 ## |--   |-- |-- (Real k1, 2.0, m, 'None')
 ## |--   |-- |-- (Real k2, 3.0, kg, 'None')
 ## |--   |-- |-- (String param3, hello, None, 'None')
-## |--   resources/myfile5.ssv
+## |--   resources/myfile7.ssv
 ## |--   |-- Parameter Bindings:
-## |--   |-- |-- (Real k1, 2.0, m, 'None')
-## |--   |-- |-- (Real k2, 3.0, kg, 'None')
-## |--   |-- |-- (String param3, hello, None, 'None')
+## |--   |-- |-- (Real k1, 200.0, m, 'None')
+## |--   |-- |-- (Real k2, 300.0, kg, 'None')
+## |--   |-- |-- (String param3, ssp, None, 'None')
 ## |-- <class 'OMSimulator.ssd.SSD'>
 ## |-- Active variant "default": <hidden>
 ## |-- |-- System: default 'None'
 ## |-- |-- |-- Connectors:
 ## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
 ## |-- |-- |-- |-- (input1, Causality.input, SignalType.Real, None, 'None')
-## |-- |-- |-- Parameter Bindings: resources/myfile3.ssv
-## |-- |-- |-- Parameter Bindings: resources/myfile4.ssv
+## |-- |-- |-- Parameter Bindings: resources/myfile5.ssv
+## |-- |-- |-- Parameter Bindings: resources/myfile6.ssv
 ## |-- |-- |-- Elements:
 ## |-- |-- |-- |-- System: sub-system 'None'
 ## |-- |-- |-- |-- |-- Connectors:
 ## |-- |-- |-- |-- |-- |-- (param2, Causality.parameter, SignalType.Real, None, 'None')
 ## |-- |-- |-- |-- |-- |-- (input2, Causality.input, SignalType.Real, None, 'None')
-## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile4.ssv
+## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile6.ssv
 ## |-- |-- |-- |-- FMU: Add1 'None'
 ## |-- |-- |-- |-- |-- path: resources/Add.fmu
 ## |-- |-- |-- |-- |-- Connectors:
@@ -110,8 +110,8 @@ model2.list()
 ## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
 ## |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
 ## |-- |-- |-- |-- |-- |-- (k2, Causality.parameter, SignalType.Real, None, 'Gain of input signal 2')
-## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile4.ssv
-## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile5.ssv
+## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile6.ssv
+## |-- |-- |-- |-- |-- Parameter Bindings: resources/myfile7.ssv
 ## |-- |-- |-- |-- FMU: Add2 'None'
 ## |-- |-- |-- |-- |-- path: resources/Add.fmu
 ## |-- |-- |-- |-- |-- Connectors:
