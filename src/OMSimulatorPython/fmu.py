3752ba09346736a5e4327e77571777e88327757c
arun3688 <rain100falls@gmail.com>
2025-05-28 13:32:41 +0200

    export ssv and ssm templates directly on FMU instance

--- src/OMSimulatorPython/fmu.py
+++ src/OMSimulatorPython/fmu.py
@@ -1,12 +1,15 @@
 import zipfile
+import logging
 from pathlib import Path
 from typing import Union
 
 from lxml import etree as ET
 from OMSimulator.connector import Connector
 from OMSimulator.unit import Unit
-from OMSimulator.variable import Variable
+from OMSimulator.variable import Variable, SignalType
+from OMSimulator import namespace
 
+logger = logging.getLogger(__name__)
 
 class FMU:
   def __init__(self, fmu_path: Union[str, Path]):
@@ -188,3 +191,69 @@ class FMU:
     '''Exports all unit definitions to an SSD XML node.'''
     for unit in self._unitDefinitions:
       unit.exportToSSD(node)
+
+  def exportSSVTemplate(self, filename: Path | None = None):
+    if filename is None:
+      filename = self.modelName + '.ssv'
+
+    ssv_node = ET.Element(namespace.tag("ssv", "ParameterSet"),
+                                   nsmap={"ssc": "http://ssp-standard.org/SSP1/SystemStructureCommon",
+                                          "ssv": "http://ssp-standard.org/SSP1/SystemStructureParameterValues"},
+                                   version = "2.0",
+                                   name = "parameters")
+    parameters_node = ET.SubElement(ssv_node, namespace.tag("ssv", "Parameters"))
+    ## extract variables with start values
+    for var in self.variables:
+      if var.isInput() or var.isParameter() or var.isCalculatedParameter():
+        parameter_node = ET.SubElement(parameters_node, namespace.tag("ssv", "Parameter"))
+        parameter_node.set("name", str(var.name))
+        if var.description:
+          parameter_node.set("description", var.description)
+
+        match var.signal_type:
+          case SignalType.Real:
+            type_tag = "Real"
+          case SignalType.Boolean:  # Check for boolean first, because it is a subclass of int
+            type_tag = "Boolean"
+          case SignalType.Integer:
+            type_tag = "Integer"
+          case SignalType.String:
+            type_tag = "String"
+          case _:
+            raise TypeError(f"Unsupported type: {type(var.signal_type)}")
+        parameter_type = ET.SubElement(parameter_node, namespace.tag("ssv", type_tag))
+        parameter_type.set("value", str(var.modelDescriptionStartValue))
+        if var.unit is not None:
+          parameter_type.set("unit", str(var.unit))
+
+
+    xml = ET.tostring(ssv_node, encoding='utf-8', xml_declaration=True, pretty_print=True).decode('utf-8')
+
+    ## write to filesystem
+    with open(Path(filename).resolve(), "w", encoding="utf-8") as file:
+      file.write(xml)
+    logger.info(f"SSV template '{filename}' successfully exported!")
+
+  def exportSSMTemplate(self, filename : Path | None = None):
+    if filename is None:
+      filename = self.modelName + '.ssm'
+
+    ssm_node = ET.Element(namespace.tag("ssm", "ParameterMapping"),
+                                   nsmap={"ssc": "http://ssp-standard.org/SSP1/SystemStructureCommon",
+                                          "ssm": "http://ssp-standard.org/SSP1/SystemStructureParameterMapping"},
+                                   version = "2.0")
+
+    for var in self.variables:
+      if var.isInput() or var.isParameter() or var.isCalculatedParameter():
+        ssm_mapping_node = ET.SubElement(ssm_node, namespace.tag("ssm", "MappingEntry"))
+        ssm_mapping_node.set("source", "")
+        ssm_mapping_node.set("target", str(var.name))
+
+
+
+    xml = ET.tostring(ssm_node, encoding='utf-8', xml_declaration=True, pretty_print=True).decode('utf-8')
+
+    ## write to filesystem
+    with open(Path(filename).resolve(), "w", encoding="utf-8") as file:
+      file.write(xml)
+    logger.info(f"SSM template '{filename}' successfully exported!")
