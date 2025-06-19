51648c241d67b9e3f7a2adccdb0f37c6be66e4f7
arun3688 <rain100falls@gmail.com>
2025-06-19 12:03:00 +0200

    expected output

--- testsuite/api/deleteSystem1.py
+++ testsuite/api/deleteSystem1.py
@@ -61,8 +61,8 @@ print("==============================")
 model2.list()
 
 ## Result:
-## ## After deleting connectors:
-## =============================
+## ## original model:
+## ===================
 ## <class 'OMSimulator.ssp.SSP'>
 ## |-- Resources:
 ## |--   resources/Add.fmu
@@ -72,13 +72,16 @@ model2.list()
 ## |-- Variant "default": <hidden>
 ## |-- |-- System: default 'None'
 ## |-- |-- |-- Connectors:
+## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
 ## |-- |-- |-- Elements:
 ## |-- |-- |-- |-- System: sub-system 'None'
 ## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (input, Causality.input, SignalType.Real, None, 'None')
 ## |-- |-- |-- |-- |-- Elements:
 ## |-- |-- |-- |-- |-- |-- FMU: Add2 'None'
 ## |-- |-- |-- |-- |-- |-- |-- path: resources/Add.fmu
 ## |-- |-- |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- |-- |-- (u1, Causality.input, SignalType.Real, None, 'Connector of Real input signal 1')
 ## |-- |-- |-- |-- |-- |-- |-- |-- (u2, Causality.input, SignalType.Real, None, 'Connector of Real input signal 2')
 ## |-- |-- |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
 ## |-- |-- |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
@@ -90,6 +93,7 @@ model2.list()
 ## |-- |-- |-- |-- FMU: Add1 'None'
 ## |-- |-- |-- |-- |-- path: resources/Add.fmu
 ## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u1, Causality.input, SignalType.Real, None, 'Connector of Real input signal 1')
 ## |-- |-- |-- |-- |-- |-- (u2, Causality.input, SignalType.Real, None, 'Connector of Real input signal 2')
 ## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
 ## |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
@@ -101,6 +105,100 @@ model2.list()
 ## |-- |-- |-- |-- Add1.y -> sub-system.input
 ## |-- |-- |-- Solver Settings:
 ## |-- |-- |-- |-- (name=solver1, method=euler, tolerance=1e-06)
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
+## ## After deleting component Add2 from sub-system:
+## =================================================
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
+## |-- |-- System: default 'None'
+## |-- |-- |-- Connectors:
+## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
+## |-- |-- |-- Elements:
+## |-- |-- |-- |-- System: sub-system 'None'
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (input, Causality.input, SignalType.Real, None, 'None')
+## |-- |-- |-- |-- |-- Connections:
+## |-- |-- |-- |-- |-- |-- Add2.u1 -> .input
+## |-- |-- |-- |-- FMU: Add1 'None'
+## |-- |-- |-- |-- |-- path: resources/Add.fmu
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u1, Causality.input, SignalType.Real, None, 'Connector of Real input signal 1')
+## |-- |-- |-- |-- |-- |-- (u2, Causality.input, SignalType.Real, None, 'Connector of Real input signal 2')
+## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
+## |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
+## |-- |-- |-- |-- |-- |-- (k2, Causality.parameter, SignalType.Real, None, 'Gain of input signal 2')
+## |-- |-- |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- |-- |-- name: solver1
+## |-- |-- |-- Connections:
+## |-- |-- |-- |-- .param1 -> Add1.u1
+## |-- |-- |-- |-- Add1.y -> sub-system.input
+## |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- (name=solver1, method=euler, tolerance=1e-06)
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
+## ## After deleting component Add1 from top level system:
+## ========================================================
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
+## |-- |-- System: default 'None'
+## |-- |-- |-- Connectors:
+## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
+## |-- |-- |-- Elements:
+## |-- |-- |-- |-- System: sub-system 'None'
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (input, Causality.input, SignalType.Real, None, 'None')
+## |-- |-- |-- |-- |-- Connections:
+## |-- |-- |-- |-- |-- |-- Add2.u1 -> .input
+## |-- |-- |-- Connections:
+## |-- |-- |-- |-- .param1 -> Add1.u1
+## |-- |-- |-- |-- Add1.y -> sub-system.input
+## |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- (name=solver1, method=euler, tolerance=1e-06)
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
+## ## After deleting sub-system:
+## ==============================
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
+## |-- |-- System: default 'None'
+## |-- |-- |-- Connectors:
+## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
+## |-- |-- |-- Connections:
+## |-- |-- |-- |-- .param1 -> Add1.u1
+## |-- |-- |-- |-- Add1.y -> sub-system.input
+## |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- (name=solver1, method=euler, tolerance=1e-06)
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
+## ## After deleting full system:
+## ==============================
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
 ## |-- DefaultExperiment
 ## |-- |-- startTime: 0.0
 ## |-- |-- stopTime: 1.0
