2cc1150fa13cf7608a6e8d736bb5eb4052a826f0
arun3688 <rain100falls@gmail.com>
2025-08-29 13:16:40 +0200

    add test

---
+++ testsuite/simulation/SimpleSimulation8.py
@@ -0,0 +1,169 @@
+## status: correct
+## teardown_command: rm -rf SimpleSimulation8.ssp SimpleSimulation8_res.mat
+## linux: yes
+## ucrt64: yes
+## win: yes
+## mac: yes
+
+from OMSimulator import SSP, CRef, Settings, Connector, Causality, SignalType
+
+Settings.suppressPath = True
+
+
+# This example creates a new SSP file with an FMU instantiated as a component and sets two differents solver for the components and the system.
+# It then exports the SSP file and re-imports it to verify the solver settings and the simulates the model.
+
+model = SSP()
+model.addResource('../resources/Modelica.Blocks.Math.Add.fmu', new_name='resources/Add.fmu')
+model.addResource('../resources/Modelica.Blocks.Math.Gain.fmu', new_name='resources/Gain.fmu')
+## add top level system connector
+model.activeVariant.system.addConnector(Connector('param1', Causality.parameter, SignalType.Real))
+model.activeVariant.system.addConnector(Connector('input1', Causality.input, SignalType.Real))
+
+## add subsystem
+model.addSystem(CRef('default', 'sub-system'))
+## add top level sub-system connector
+model.activeVariant.system.elements[CRef('sub-system')].addConnector(Connector('input', Causality.input, SignalType.Real))
+
+component1 = model.addComponent(CRef('default', 'Add1'), 'resources/Add.fmu')
+component2 = model.addComponent(CRef('default', 'Gain1'), 'resources/Gain.fmu')
+
+component3 = model.addComponent(CRef('default', 'sub-system', 'Gain2'), 'resources/Gain.fmu')
+
+solver2 = {'name' : 'solver2',  'method': 'cvode', 'tolerance': 1e-4}
+model.newSolver(solver2)
+model.setSolver(CRef('default', 'sub-system', 'Gain2'), 'solver2')
+
+model.setValue(CRef('default','param1'), 200.0)
+model.setValue(CRef('default','input1'), 300.0)
+model.setValue(CRef('default', 'Gain1', 'k'), 2.0)
+model.setValue(CRef('default','sub-system','input'), 400.0)
+
+## System.Input to Element.Input
+model.addConnection(CRef('default', 'input1'), CRef('default', 'Gain1', 'u'))
+model.addConnection(CRef('default', 'Gain1', 'y'), CRef('default', 'Add1', 'u1'))
+
+## Sub-system.Input to Element.Input
+model.addConnection(CRef('default', 'sub-system', 'input'), CRef('default', 'sub-system', 'Gain2', 'u'))
+model.export('SimpleSimulation8.ssp')
+
+model2 = SSP('SimpleSimulation8.ssp')
+model2.list()
+instantiated_model = model2.instantiate() ## internally generate the json file and also set the model state like virgin,
+#print(instantiated_model.dumpApiCalls(), flush=True)
+
+print(f"info: After instantiation:")
+print(f"info:    default.param1 : {instantiated_model.getValue(CRef('default', 'param1'))}", flush=True)
+print(f"info:    default.input1 : {instantiated_model.getValue(CRef('default', 'input1'))}", flush=True)
+print(f"info:    default.Gain1.k: {instantiated_model.getValue(CRef('default', 'Gain1', 'k'))}", flush=True)
+print(f"info:    default.Gain1.u: {instantiated_model.getValue(CRef('default', 'Gain1', 'u'))}", flush=True)
+print(f"info:    default.Gain1.y: {instantiated_model.getValue(CRef('default', 'Gain1', 'y'))}", flush=True)
+print(f"info:    default.Add1.u1: {instantiated_model.getValue(CRef('default', 'Add1', 'u1'))}", flush=True)
+print(f"info:    default.sub-system.input: {instantiated_model.getValue(CRef('default', 'sub-system', 'input'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.k: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'k'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.u: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'u'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.y: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'y'))}", flush=True)
+
+
+instantiated_model.initialize()
+instantiated_model.simulate()
+print(f"info: After simulation:")
+print(f"info:    default.param1 : {instantiated_model.getValue(CRef('default', 'param1'))}", flush=True)
+print(f"info:    default.input1 : {instantiated_model.getValue(CRef('default', 'input1'))}", flush=True)
+print(f"info:    default.Gain1.k: {instantiated_model.getValue(CRef('default', 'Gain1', 'k'))}", flush=True)
+print(f"info:    default.Gain1.u: {instantiated_model.getValue(CRef('default', 'Gain1', 'u'))}", flush=True)
+print(f"info:    default.Gain1.y: {instantiated_model.getValue(CRef('default', 'Gain1', 'y'))}", flush=True)
+print(f"info:    default.Add1.u1: {instantiated_model.getValue(CRef('default', 'Add1', 'u1'))}", flush=True)
+print(f"info:    default.sub-system.input: {instantiated_model.getValue(CRef('default', 'sub-system', 'input'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.k: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'k'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.u: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'u'))}", flush=True)
+print(f"info:    default.sub-system.Gain2.y: {instantiated_model.getValue(CRef('default', 'sub-system', 'Gain2', 'y'))}", flush=True)
+
+instantiated_model.terminate()
+instantiated_model.delete()
+
+## Result:
+## info:    model doesn't contain any continuous state
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
+## |-- |-- System: default 'None'
+## |-- |-- |-- Connectors:
+## |-- |-- |-- |-- (param1, Causality.parameter, SignalType.Real, None, 'None')
+## |-- |-- |-- |-- (input1, Causality.input, SignalType.Real, None, 'None')
+## |-- |-- |-- Inline Parameter Bindings:
+## |-- |-- |-- |-- (Real param1, 200.0, None, 'None')
+## |-- |-- |-- |-- (Real input1, 300.0, None, 'None')
+## |-- |-- |-- Elements:
+## |-- |-- |-- |-- System: sub-system 'None'
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (input, Causality.input, SignalType.Real, None, 'None')
+## |-- |-- |-- |-- |-- Inline Parameter Bindings:
+## |-- |-- |-- |-- |-- |-- (Real input, 400.0, None, 'None')
+## |-- |-- |-- |-- |-- Elements:
+## |-- |-- |-- |-- |-- |-- FMU: Gain2 'None'
+## |-- |-- |-- |-- |-- |-- |-- path: resources/Gain.fmu
+## |-- |-- |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- |-- |-- (u, Causality.input, SignalType.Real, None, 'Input signal connector')
+## |-- |-- |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Output signal connector')
+## |-- |-- |-- |-- |-- |-- |-- |-- (k, Causality.parameter, SignalType.Real, 1, 'Gain value multiplied with input signal')
+## |-- |-- |-- |-- |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- |-- |-- |-- |-- name: solver2
+## |-- |-- |-- |-- |-- Connections:
+## |-- |-- |-- |-- |-- |-- .input -> Gain2.u
+## |-- |-- |-- |-- FMU: Add1 'None'
+## |-- |-- |-- |-- |-- path: resources/Add.fmu
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u1, Causality.input, SignalType.Real, None, 'Connector of Real input signal 1')
+## |-- |-- |-- |-- |-- |-- (u2, Causality.input, SignalType.Real, None, 'Connector of Real input signal 2')
+## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
+## |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
+## |-- |-- |-- |-- |-- |-- (k2, Causality.parameter, SignalType.Real, None, 'Gain of input signal 2')
+## |-- |-- |-- |-- FMU: Gain1 'None'
+## |-- |-- |-- |-- |-- path: resources/Gain.fmu
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u, Causality.input, SignalType.Real, None, 'Input signal connector')
+## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Output signal connector')
+## |-- |-- |-- |-- |-- |-- (k, Causality.parameter, SignalType.Real, 1, 'Gain value multiplied with input signal')
+## |-- |-- |-- |-- |-- Inline Parameter Bindings:
+## |-- |-- |-- |-- |-- |-- (Real k, 2.0, None, 'None')
+## |-- |-- |-- Connections:
+## |-- |-- |-- |-- .input1 -> Gain1.u
+## |-- |-- |-- |-- Gain1.y -> Add1.u1
+## |-- |-- |-- Solver Settings:
+## |-- |-- |-- |-- (name=solver2, method=cvode, tolerance=0.0001)
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
+## info: After instantiation:
+## info:    default.param1 : 200.0
+## info:    default.input1 : 300.0
+## info:    default.Gain1.k: 2.0
+## info:    default.Gain1.u: 0.0
+## info:    default.Gain1.y: 0.0
+## info:    default.Add1.u1: 0.0
+## info:    default.sub-system.input: 400.0
+## info:    default.sub-system.Gain2.k: 1.0
+## info:    default.sub-system.Gain2.u: 0.0
+## info:    default.sub-system.Gain2.y: 0.0
+## info:    maximum step size for 'model.root.solver2': 0.001000
+## info:    Result file: model_res.mat (bufferSize=10)
+## info: After simulation:
+## info:    default.param1 : 200.0
+## info:    default.input1 : 300.0
+## info:    default.Gain1.k: 2.0
+## info:    default.Gain1.u: 300.0
+## info:    default.Gain1.y: 600.0
+## info:    default.Add1.u1: 600.0
+## info:    default.sub-system.input: 400.0
+## info:    default.sub-system.Gain2.k: 1.0
+## info:    default.sub-system.Gain2.u: 400.0
+## info:    default.sub-system.Gain2.y: 400.0
+## info:    Final Statistics for 'model.root.solver2':
+##          NumSteps = 1001 NumRhsEvals  = 1002 NumLinSolvSetups = 51
+##          NumNonlinSolvIters = 1001 NumNonlinSolvConvFails = 0 NumErrTestFails = 0
+## endResult
