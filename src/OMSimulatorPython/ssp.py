60cdbd46942a3af5790c6ad38859b3c513e14c77
Robert Braun <robert.braun@liu.se>
2025-09-08 15:34:45 +0200

    Initial DCP support (wip)

--- src/OMSimulatorPython/ssp.py
+++ src/OMSimulatorPython/ssp.py
@@ -11,6 +11,7 @@ from OMSimulator.settings import suppress_path_to_str
 from OMSimulator.ssv import SSV
 from OMSimulator.ssm import SSM
 from OMSimulator.componenttable import ResultReader
+from OMSimulator.dcp import DCP
 
 from OMSimulator import SSD, CRef, namespace
 from lxml import etree as ET
@@ -112,6 +113,8 @@ class SSP:
       self.resources[str(new_name)] = SSM(ssm_path = filePath)
     elif Path(filename).suffix == ".csv" or Path(filename).suffix == ".mat":
       self.resources[str(new_name)] = ResultReader(filePath = filePath)
+    elif Path(filename).suffix == ".dcp":
+      self.resources[str(new_name)] = DCP(dcp_path = filePath)
     ##TODO check for .ssv file and if ssv instances provided
     else:
       self.resources[Path(filename).name] = new_name
