f0f624ea60f7ac5dccaa03bb7f93934843be2a09
arun3688 <rain100falls@gmail.com>
2025-09-09 11:34:05 +0200

    fix causality ordering with C API

--- src/OMSimulatorPython/variable.py
+++ src/OMSimulatorPython/variable.py
@@ -6,10 +6,10 @@ from OMSimulator.cref import CRef
 
 class Causality(Enum):
   '''Enumeration for signal causality (oms_causality_enu_t).'''
-  parameter = 0
-  calculatedParameter = 1
-  input = 2
-  output = 3
+  input = 0
+  output = 1
+  parameter = 2
+  calculatedParameter = 3
   local = 4
   independent = 5
 
