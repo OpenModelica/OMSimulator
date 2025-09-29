0f30534cd721ab247208f682184911b5905791e7
arun3688 <rain100falls@gmail.com>
2025-09-29 20:59:03 +0200

    expected output

--- testsuite/api/invalidSSD.py
+++ testsuite/api/invalidSSD.py
@@ -23,7 +23,7 @@ except Exception as e:
 
 ## Result:
 ## INFO:OMSimulator.ssp:Temporary directory created: <hidden>
-## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
-## Error: Failed to validate SystemStructure.ssd against schemafile SystemStructureDescription.xsd
+## UserWarning: Failed to validate SystemStructure.ssd against schemafile SystemStructureDescription.xsd
 ## ERROR (line 16, column 0): Element '{http://ssp-standard.org/SSP1/SystemStructureDescription}Connector', attribute 'type': The attribute 'type' is not allowed.
+## INFO:OMSimulator.ssp:Temporary directory removed: <hidden>
 ## endResult
