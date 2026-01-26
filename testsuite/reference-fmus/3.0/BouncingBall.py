e64c3a28553c0a3b250bb40a8352a2614017d55e
arun3688 <rain100falls@gmail.com>
2026-01-26 09:40:24 +0100

    fix FMI-3.0 ME tests

--- testsuite/reference-fmus/3.0/BouncingBall.py
+++ testsuite/reference-fmus/3.0/BouncingBall.py
@@ -75,4 +75,12 @@ else:
 ## info:    Result file: BouncingBall-cs.mat (bufferSize=1)
 ## signal h is equal
 ## signal v is equal
+## Loading FMI version 3...
+## info:    maximum step size for 'model.root': 0.001000
+## info:    Result file: BouncingBall-me.mat (bufferSize=1)
+## info:    Final Statistics for 'model.root':
+##          NumSteps = 503 NumRhsEvals  = 504 NumLinSolvSetups = 27
+##          NumNonlinSolvIters = 503 NumNonlinSolvConvFails = 0 NumErrTestFails = 0
+## signal h is equal
+## signal v is equal
 ## endResult
