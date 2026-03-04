1a5f4bf0d5c3b5d4893379502f7e02845d41164a
arun3688 <rain100falls@gmail.com>
2026-03-04 20:46:38 +0100

    expected output

--- testsuite/simulation/LinearTransformation3.py
+++ testsuite/simulation/LinearTransformation3.py
@@ -38,6 +38,50 @@ instantiated_model.delete()
 
 
 ## Result:
+## <class 'OMSimulator.ssp.SSP'>
+## |-- Resources:
+## |--   resources/Add.fmu
+## |--   resources/Gain.fmu
+## |--   resources/mappedEntry1.ssm
+## |--   |-- Parameter Mapping:
+## |--   |-- |-- source: mapped_Gain_u
+## |--   |-- |-- |-- targets: [('Gain1.u', LinearTransformation (factor: 2, offset: 10))]
+## |--   |-- |-- source: mapped_Gain_k
+## |--   |-- |-- |-- targets: [('Gain1.k', LinearTransformation (factor: 4, offset: 5))]
+## |--   resources/mappedParameter1.ssv
+## |--   |-- Parameter Bindings:
+## |--   |-- |-- (Real mapped_Gain_u, 2.0, None, 'None')
+## |--   |-- |-- (Real mapped_Gain_k, 2.0, None, 'None')
+## |-- Active Variant: default
+## |-- <class 'OMSimulator.ssd.SSD'>
+## |-- Variant "default": <hidden>
+## |-- |-- System: default 'None'
+## |-- |-- |-- Connectors:
+## |-- |-- |-- Parameter Bindings: resources/mappedParameter1.ssv
+## |-- |-- |-- |-- Parameter Mapping: resources/mappedEntry1.ssm
+## |-- |-- |-- Elements:
+## |-- |-- |-- |-- FMU: Add1 'None'
+## |-- |-- |-- |-- |-- path: resources/Add.fmu
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u1, Causality.input, SignalType.Real, None, 'Connector of Real input signal 1')
+## |-- |-- |-- |-- |-- |-- (u2, Causality.input, SignalType.Real, None, 'Connector of Real input signal 2')
+## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Connector of Real output signal')
+## |-- |-- |-- |-- |-- |-- (k1, Causality.parameter, SignalType.Real, None, 'Gain of input signal 1')
+## |-- |-- |-- |-- |-- |-- (k2, Causality.parameter, SignalType.Real, None, 'Gain of input signal 2')
+## |-- |-- |-- |-- FMU: Gain1 'None'
+## |-- |-- |-- |-- |-- path: resources/Gain.fmu
+## |-- |-- |-- |-- |-- Connectors:
+## |-- |-- |-- |-- |-- |-- (u, Causality.input, SignalType.Real, None, 'Input signal connector')
+## |-- |-- |-- |-- |-- |-- (y, Causality.output, SignalType.Real, None, 'Output signal connector')
+## |-- |-- |-- |-- |-- |-- (k, Causality.parameter, SignalType.Real, 1, 'Gain value multiplied with input signal')
+## |-- |-- |-- Connections:
+## |-- |-- |-- |-- Gain1.y -> Add1.u1
+## |-- UnitDefinitions:
+## |-- |-- Unit: 1
+## |-- |-- |-- BaseUnit:
+## |-- DefaultExperiment
+## |-- |-- startTime: 0.0
+## |-- |-- stopTime: 1.0
 ## info: After instantiation:
 ## info:    default.Gain1.k: 13.0
 ## info:    default.Gain1.u: 14.0
