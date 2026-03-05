cdd1f4cce326cfd46455268a1f8dc10cbd00228a
arun3688 <rain100falls@gmail.com>
2026-03-05 11:24:19 +0100

    initialize with values

--- testsuite/simulation/SimpleSimulation3.py
+++ testsuite/simulation/SimpleSimulation3.py
@@ -45,7 +45,10 @@ model2 = SSP('SimpleSimulation3.ssp')
 model2.list()
 print("", flush=True)
 instantiated_model = model2.instantiate() ## internally generate the json file and also set the model state like virgin,
-print(instantiated_model.dumpApiCalls(), flush=True)
+instantiated_model.setValue(CRef('default', 'Add1', 'u1'), 0.0)
+instantiated_model.setValue(CRef('default', 'Add1', 'u2'), 0.0)
+instantiated_model.setValue(CRef('default', 'Add2', 'u1'), 0.0)
+instantiated_model.setValue(CRef('default', 'Add2', 'u2'), 0.0)
 instantiated_model.setResultFile("SimpleSimulation3_res.mat")
 
 instantiated_model.initialize()
