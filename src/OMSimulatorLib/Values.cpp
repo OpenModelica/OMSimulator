a3b2fc2719772b6f3076964614fd66e4fe5b8d9b
Robert Braun <robert.braun@liu.se>
2026-05-12 10:42:33 +0200

    DCP simulation (wip)

--- src/OMSimulatorLib/Values.cpp
+++ src/OMSimulatorLib/Values.cpp
@@ -38,7 +38,7 @@
 #include "Logging.h"
 #include "ssd/Tags.h"
 #include "Util.h"
-#include "XercesValidator.h"
+//#include "XercesValidator.h"
 #include "dcp/zip/DcpSlaveReader.hpp"
 
 #include <iostream>
