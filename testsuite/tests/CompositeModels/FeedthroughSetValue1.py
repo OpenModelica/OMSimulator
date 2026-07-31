8cfb221aaa6f093d0bda78dfee4cc56bb871f2fe
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2026-07-31 12:31:57 +0200

    Remove numpy usage from testsuite
    * It was unused and leaking memory

--- testsuite/tests/CompositeModels/FeedthroughSetValue1.py
+++ testsuite/tests/CompositeModels/FeedthroughSetValue1.py
@@ -6,7 +6,6 @@
 ## asan: yes
 
 
-from numpy import uint64
 from OMSimulator import SSP, CRef, Settings, Float64, Float32, Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64
 
 Settings.suppressPath = True
